b8a6485683ffeefcef3e5446b0ef30f12b79ec24
gacarrillor <geotux_tuxman@linuxmail.org>
2017-12-12 12:26:39 -0500

    Add dummy test...

---
+++ Asistente-LADM_COL/tests/test_dummy.py
@@ -0,0 +1,11 @@
+
+import nose2
+from qgis.testing import unittest
+
+class TestExport(unittest.TestCase):
+
+    def test_export_geopackage(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    nose2.main()
