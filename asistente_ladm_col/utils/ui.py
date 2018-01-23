25a465fe8438087d016b02d97d1d2bba562f0af6
seralra96 <seralra96@gmail.com>
2018-01-23 14:44:18 -0500

    Fix plot_cadaster wizard

--- asistente_ladm_col/utils/ui.py
+++ asistente_ladm_col/utils/ui.py
@@ -1,6 +1,7 @@
 import os
 
 from qgis.PyQt.uic import loadUiType
+from qgis.PyQt.QtWidgets import QWizard
 
 def get_ui_class(ui_file):
     """Get UI Python class from .ui file.
@@ -18,3 +19,11 @@ def get_ui_class(ui_file):
             )
     )
     return loadUiType(ui_file_path)[0]
+
+def disable_next_wizard(self):
+    btnList = [QWizard.HelpButton, QWizard.Stretch, QWizard.FinishButton, QWizard.CancelButton]
+    self.setButtonLayout(btnList)
+
+def enable_next_wizard(self):
+    btnList = [QWizard.HelpButton, QWizard.Stretch, QWizard.NextButton, QWizard.FinishButton, QWizard.CancelButton]
+    self.setButtonLayout(btnList)
