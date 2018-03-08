b58f6b49f665f8b21dc579b74ce3476d5a94033c
seralra96 <seralra96@gmail.com>
2018-03-08 15:08:40 -0500

    Add initial RRR functionalities

--- asistente_ladm_col/gui/create_restriction_cadastre_wizard.py
+++ asistente_ladm_col/gui/create_restriction_cadastre_wizard.py
@@ -16,16 +16,22 @@
  *                                                                         *
  ***************************************************************************/
 """
-#from qgis.core import (QgsProject, QgsVectorLayer, QgsVectorLayerUtils,
-                       #QgsFeature, QgsMapLayerProxyModel, QgsWkbTypes)
+from qgis.core import QgsEditFormConfig, QgsVectorLayerUtils, Qgis, QgsWkbTypes
 from qgis.gui import QgsMessageBar
 from qgis.PyQt.QtCore import Qt, QPoint, QCoreApplication
 from qgis.PyQt.QtWidgets import QAction, QWizard
 
+
 from ..utils import get_ui_class
 #from ..utils.qt_utils import enable_next_wizard, disable_next_wizard
 from ..config.table_mapping_config import (
-    PLOT_TABLE,
+    RESTRICTION_TABLE,
+    RESTRICTION_TYPE_TABLE,
+    NATURAL_PARTY_TABLE,
+    LEGAL_PARTY_TABLE,
+    PARCEL_TABLE,
+    LA_BAUNIT_TABLE,
+    LA_GROUP_PARTY,
     VIDA_UTIL_FIELD_BOUNDARY_TABLE
 )
 
@@ -39,3 +45,42 @@ class CreateRestrictionCadastreWizard(QWizard, WIZARD_UI):
         self._plot_layer = None
         self._db = db
         self.qgis_utils = qgis_utils
+
+        self.button(QWizard.FinishButton).clicked.connect(self.prepare_restriction_creation)
+
+    def prepare_restriction_creation(self):
+        # Load layers
+        res_layers = self.qgis_utils.get_layers(self._db, {
+            RESTRICTION_TABLE: {'name':RESTRICTION_TABLE, 'geometry':None},
+            RESTRICTION_TYPE_TABLE: {'name':RESTRICTION_TYPE_TABLE, 'geometry':None},
+            NATURAL_PARTY_TABLE: {'name':NATURAL_PARTY_TABLE, 'geometry':None},
+            LEGAL_PARTY_TABLE:{'name':LEGAL_PARTY_TABLE, 'geometry':None},
+            PARCEL_TABLE:{'name':PARCEL_TABLE, 'geometry':None},
+            LA_BAUNIT_TABLE:{'name':LA_BAUNIT_TABLE, 'geometry':None},
+            LA_GROUP_PARTY:{'name':LA_GROUP_PARTY, 'geometry':None}}, load=True)
+
+        self._restriction_layer = res_layers[RESTRICTION_TABLE]
+        if self._restriction_layer is None:
+            self.iface.messageBar().pushMessage("Asistente LADM_COL",
+                QCoreApplication.translate("CreateAdministrativeSourceCadastreWizard",
+                                           "Administrative Source layer couldn't be found..."),
+                Qgis.Warning)
+            return
+
+        # Configure automatic fields
+        self.qgis_utils.configureAutomaticField(self._restriction_layer, VIDA_UTIL_FIELD_BOUNDARY_TABLE, "now()")
+
+        # Configure relation fields
+
+        # Don't suppress (i.e., show) feature form
+        form_config = self._restriction_layer.editFormConfig()
+        form_config.setSuppress(QgsEditFormConfig.SuppressOff)
+        self._restriction_layer.setEditFormConfig(form_config)
+
+        self.edit_spatial_source()
+
+    def edit_spatial_source(self):
+        # Open Form
+        self.iface.layerTreeView().setCurrentLayer(self._restriction_layer)
+        self._restriction_layer.startEditing()
+        self.iface.actionAddFeature().trigger()
--- asistente_ladm_col/gui/create_right_cadastre_wizard.py
+++ asistente_ladm_col/gui/create_right_cadastre_wizard.py
@@ -16,16 +16,22 @@
  *                                                                         *
  ***************************************************************************/
 """
-#from qgis.core import (QgsProject, QgsVectorLayer, QgsVectorLayerUtils,
-                       #QgsFeature, QgsMapLayerProxyModel, QgsWkbTypes)
+from qgis.core import QgsEditFormConfig, QgsVectorLayerUtils, Qgis, QgsWkbTypes
 from qgis.gui import QgsMessageBar
 from qgis.PyQt.QtCore import Qt, QPoint, QCoreApplication
 from qgis.PyQt.QtWidgets import QAction, QWizard
 
+
 from ..utils import get_ui_class
 #from ..utils.qt_utils import enable_next_wizard, disable_next_wizard
 from ..config.table_mapping_config import (
-    PLOT_TABLE,
+    RIGHT_TABLE,
+    RIGHT_TYPE_TABLE,
+    NATURAL_PARTY_TABLE,
+    LEGAL_PARTY_TABLE,
+    PARCEL_TABLE,
+    LA_BAUNIT_TABLE,
+    LA_GROUP_PARTY,
     VIDA_UTIL_FIELD_BOUNDARY_TABLE
 )
 
@@ -39,3 +45,42 @@ class CreateRightCadastreWizard(QWizard, WIZARD_UI):
         self._plot_layer = None
         self._db = db
         self.qgis_utils = qgis_utils
+
+        self.button(QWizard.FinishButton).clicked.connect(self.prepare_right_creation)
+
+    def prepare_right_creation(self):
+        # Load layers
+        res_layers = self.qgis_utils.get_layers(self._db, {
+            RIGHT_TABLE: {'name':RIGHT_TABLE, 'geometry':None},
+            RIGHT_TYPE_TABLE: {'name':RIGHT_TYPE_TABLE, 'geometry':None},
+            NATURAL_PARTY_TABLE: {'name':NATURAL_PARTY_TABLE, 'geometry':None},
+            LEGAL_PARTY_TABLE:{'name':LEGAL_PARTY_TABLE, 'geometry':None},
+            PARCEL_TABLE:{'name':PARCEL_TABLE, 'geometry':None},
+            LA_BAUNIT_TABLE:{'name':LA_BAUNIT_TABLE, 'geometry':None},
+            LA_GROUP_PARTY:{'name':LA_GROUP_PARTY, 'geometry':None}}, load=True)
+
+        self._right_layer = res_layers[RIGHT_TABLE]
+        if self._right_layer is None:
+            self.iface.messageBar().pushMessage("Asistente LADM_COL",
+                QCoreApplication.translate("CreateAdministrativeSourceCadastreWizard",
+                                           "Administrative Source layer couldn't be found..."),
+                Qgis.Warning)
+            return
+
+        # Configure automatic fields
+        self.qgis_utils.configureAutomaticField(self._right_layer, VIDA_UTIL_FIELD_BOUNDARY_TABLE, "now()")
+
+        # Configure relation fields
+
+        # Don't suppress (i.e., show) feature form
+        form_config = self._right_layer.editFormConfig()
+        form_config.setSuppress(QgsEditFormConfig.SuppressOff)
+        self._right_layer.setEditFormConfig(form_config)
+
+        self.edit_spatial_source()
+
+    def edit_spatial_source(self):
+        # Open Form
+        self.iface.layerTreeView().setCurrentLayer(self._right_layer)
+        self._right_layer.startEditing()
+        self.iface.actionAddFeature().trigger()
