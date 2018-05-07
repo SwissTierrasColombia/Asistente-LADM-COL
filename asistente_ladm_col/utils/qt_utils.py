f67fa405a20cec2f93bd6dc31d7496df490af7c5
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-05-07 14:55:32 -0500

    Detect path from sys modules if it is not loaded from qgis.utils.plugins

--- asistente_ladm_col/utils/qt_utils.py
+++ asistente_ladm_col/utils/qt_utils.py
@@ -19,7 +19,7 @@
 """
 
 import os.path
-import inspect
+import sys
 
 import qgis
 from qgis.PyQt.QtWidgets import (
@@ -83,7 +83,12 @@ def enable_next_wizard(wizard, with_back=True):
 
 
 def get_plugin_metadata(plugin_name, key):
-    file_path = os.path.join(qgis.utils.plugins[plugin_name].plugin_dir, 'metadata.txt')
+    plugin_dir = None
+    if plugin_name in qgis.utils.plugins:
+        plugin_dir = qgis.utils.plugins[plugin_name].plugin_dir
+    else:
+        plugin_dir = os.path.dirname(sys.modules[plugin_name].__file__)
+    file_path = os.path.join(plugin_dir, 'metadata.txt')
     if os.path.isfile(file_path):
         with open(file_path) as metadata:
             for line in metadata:
