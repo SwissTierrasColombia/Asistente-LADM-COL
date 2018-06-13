0b804175130def910713359458183c59c3238be6
grand <leocardonapiedrahita@gmail.com>
2018-06-13 16:16:35 -0500

    ajust test overlapping 3 polygons

--- asistente_ladm_col/tests/test_quality_validations.py
+++ asistente_ladm_col/tests/test_quality_validations.py
@@ -186,14 +186,14 @@ class TesQualityValidations(unittest.TestCase):
             polygons_overlap_layer = processing.run("native:multiparttosingleparts",
                                            {'INPUT': polygons_overlap_layer, 'OUTPUT': 'memory:'})['OUTPUT']
 
-        expected_overlaps = [[11, 44], [11, 47], [12, 44], [12, 45], [12, 57], [48, 49], [53, 55], [61, 62]]
+        expected_overlaps = [[11, 44], [11, 47], [12, 44], [12, 45], [12, 57], [48, 49], [53, 55], [61, 62], [63, 64], [63, 65], [64, 65]]
         flat_expected_overlaps = list(set([id for items in expected_overlaps for id in items]))  # Build a flat list of uniques ids
 
         overlapping = self.qgis_utils.geometry.get_overlapping_polygons(polygons_overlap_layer)
         flat_overlapping = list(set([id for items in overlapping for id in items]))
 
         # checks
-        self.assertEqual(len(flat_overlapping), 12)
+        self.assertEqual(len(flat_overlapping), 15)
         self.assertEqual(flat_expected_overlaps.sort(), flat_overlapping.sort())
 
     def test_intersection_polygons_tolerance(self):
