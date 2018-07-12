38864fb0cd9d0f8c63f7b356f0bfdff569e1e739
gacarrillor <gcarrillo@linuxmail.org>
2018-07-12 10:37:58 -0500

    [gui] Handle error due to a too early QGIS version

--- asistente_ladm_col/__init__.py
+++ asistente_ladm_col/__init__.py
@@ -16,6 +16,21 @@
  *                                                                         *
  ***************************************************************************/
 """
+from qgis.PyQt.QtCore import QCoreApplication
+
 def classFactory(iface):
-    from .asistente_ladm_col_plugin import AsistenteLADMCOLPlugin
+    try:
+        from .asistente_ladm_col_plugin import AsistenteLADMCOLPlugin
+    except ImportError as e:
+        iface.messageBar().pushMessage("Asistente LADM_COL",
+            QCoreApplication.translate("__init__",
+               "The plugin Asistente LADM_COL requires a newer version of QGIS! \
+               If you're on Windows, you can download a recent version from \
+               <a target=\"_blank\" href=\"https://qgis.org/downloads/weekly/\">this link</a>. \
+               Otherwise, use a <a href=\"https://qgis.org/en/site/forusers/download.html\" target=\"_blank\">nightly build</a>."),
+            1, 0) # We don't use Qgis.Info because it was introduced after 2.99
+
+        from mock import Mock
+        return Mock()
+
     return AsistenteLADMCOLPlugin(iface)
