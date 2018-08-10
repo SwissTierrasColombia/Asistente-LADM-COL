800772eed51fd0d9c520ec4f46273e3798a95d30
Sergio Ramirez <seralra96@gmail.com>
2018-08-10 17:09:27 -0500

    Initial group party interface

---
+++ asistente_ladm_col/gui/create_group_party_cadastre.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                              Asistente LADM_COL
+                             --------------------
+        begin                : 2018-08-10
+        git sha              : :%H$
+        copyright            : (C) 2018 by Sergio Ramírez (Incige SAS)
+        email                : seralra96@gmail.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License v3.0 as          *
+ *   published by the Free Software Foundation.                            *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from qgis.core import (QgsEditFormConfig, QgsVectorLayerUtils, Qgis,
+                       QgsWkbTypes, QgsMapLayerProxyModel)
+from qgis.gui import QgsMessageBar
+from qgis.PyQt.QtCore import Qt, QPoint, QCoreApplication, QSettings
+from qgis.PyQt.QtWidgets import QDialog
+
+from ..utils import get_ui_class
+from ..config.table_mapping_config import NATURAL_PARTY_TABLE
+from ..config.help_strings import HelpStrings
+
+DIALOG_UI = get_ui_class('dlg_group_party.ui')
+
+class CreateGroupPartyCadastre(QDialog, DIALOG_UI):
+    def __init__(self, iface, db, qgis_utils, parent=None):
+        QDialog.__init__(self)
+        self.setupUi(self)
+        self.iface = iface
+        self._natural_party_layer = None
+        self._db = db
+        self.qgis_utils = qgis_utils
+        self.help_strings = HelpStrings()
+
+        #self.restore_settings()
+    #
+    #     self.rad_create_manually.toggled.connect(self.adjust_page_1_controls)
+    #     self.adjust_page_1_controls()
+    #     self.button(QWizard.FinishButton).clicked.connect(self.finished_dialog)
+    #     self.button(QWizard.HelpButton).clicked.connect(self.show_help)
+    #
+    #     self.mMapLayerComboBox.setFilters(QgsMapLayerProxyModel.NoGeometry)
+    #
+    # def adjust_page_1_controls(self):
+    #     if self.rad_refactor.isChecked():
+    #         self.lbl_refactor_source.setEnabled(True)
+    #         self.mMapLayerComboBox.setEnabled(True)
+    #         finish_button_text = QCoreApplication.translate("CreateNaturalPartyCadastreWizard", "Import")
+    #         self.txt_help_page_1.setHtml(self.help_strings.get_refactor_help_string(NATURAL_PARTY_TABLE, False))
+    #
+    #     elif self.rad_create_manually.isChecked():
+    #         self.lbl_refactor_source.setEnabled(False)
+    #         self.mMapLayerComboBox.setEnabled(False)
+    #         finish_button_text = QCoreApplication.translate("CreateNaturalPartyCadastreWizard", "Create")
+    #         self.txt_help_page_1.setHtml(self.help_strings.WIZ_CREATE_NATURAL_PARTY_CADASTRE_PAGE_1_OPTION_FORM)
+    #
+    #     self.wizardPage1.setButtonText(QWizard.FinishButton,
+    #                                    QCoreApplication.translate("CreateNaturalPartyCadastreWizard",
+    #                                    finish_button_text))
+    #
+    # def finished_dialog(self):
+    #     self.save_settings()
+    #
+    #     if self.rad_refactor.isChecked():
+    #         if self.mMapLayerComboBox.currentLayer() is not None:
+    #             self.qgis_utils.show_etl_model(self._db,
+    #                                            self.mMapLayerComboBox.currentLayer(),
+    #                                            NATURAL_PARTY_TABLE)
+    #         else:
+    #             self.iface.messageBar().pushMessage("Asistente LADM_COL",
+    #                 QCoreApplication.translate("CreateNaturalPartyCadastreWizard",
+    #                                            "Select a source layer to set the field mapping to '{}'.").format(NATURAL_PARTY_TABLE),
+    #                 Qgis.Warning)
+    #
+    #     elif self.rad_create_manually.isChecked():
+    #         self.prepare_natural_party_creation()
+    #
+    # def prepare_natural_party_creation(self):
+    #     # Load layers
+    #     self._natural_party_layer = self.qgis_utils.get_layer(self._db, NATURAL_PARTY_TABLE, load=True)
+    #     if self._natural_party_layer is None:
+    #         self.iface.messageBar().pushMessage("Asistente LADM_COL",
+    #             QCoreApplication.translate("CreateNaturalPartyCadastreWizard",
+    #                                        "Natural Party layer couldn't be found... {}").format(self._db.get_description()),
+    #             Qgis.Warning)
+    #         return
+    #
+    #     # Don't suppress (i.e., show) feature form
+    #     form_config = self._natural_party_layer.editFormConfig()
+    #     form_config.setSuppress(QgsEditFormConfig.SuppressOff)
+    #     self._natural_party_layer.setEditFormConfig(form_config)
+    #
+    #     self.edit_natural_party()
+    #
+    # def edit_natural_party(self):
+    #     # Open Form
+    #     self.iface.layerTreeView().setCurrentLayer(self._natural_party_layer)
+    #     self._natural_party_layer.startEditing()
+    #     self.iface.actionAddFeature().trigger()
+
+    # def save_settings(self):
+    #     settings = QSettings()
+    #     settings.setValue('Asistente-LADM_COL/wizards/natural_party_load_data_type', 'create_manually' if self.rad_create_manually.isChecked() else 'refactor')
+    #
+    # def restore_settings(self):
+    #     settings = QSettings()
+    #
+    #     load_data_type = settings.value('Asistente-LADM_COL/wizards/natural_party_load_data_type') or 'create_manually'
+    #     if load_data_type == 'refactor':
+    #         self.rad_refactor.setChecked(True)
+    #     else:
+    #         self.rad_create_manually.setChecked(True)
+
+    def show_help(self):
+        self.qgis_utils.show_help("natural_party")
