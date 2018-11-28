572311b206a34dd6eb596c4720d4d595508b298a
gacarrillor <gcarrillo@linuxmail.org>
2018-11-28 14:03:52 -0500

    [i18n] Use a default language variable in the plugin (and for symbology of 'plots should be covered by boundaries and viceversa' rules)

--- asistente_ladm_col/config/translator.py
+++ asistente_ladm_col/config/translator.py
@@ -4,13 +4,15 @@ from qgis.PyQt.QtCore import (QLocale,
                               QCoreApplication,
                               QTranslator)
 
+DEFAULT_LANGUAGE = 'en'
+
 try:
     # Errors here could happen if the value cannot be converted to string or
     # if it is not subscriptable (see https://github.com/gacarrillor/loadthemall/issues/11)
     locale = QSettings().value("locale/userLocale", type=str)
     QGIS_LANG = str( locale[:2] )
 except TypeError as e:
-    QGIS_LANG = 'en'
+    QGIS_LANG = DEFAULT_LANGUAGE
 PLUGIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Install Qt Translator
