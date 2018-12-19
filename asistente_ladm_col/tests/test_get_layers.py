08e7472bffeab81c9736965b50aee568bf41f3df
Fernando Pineda <wfpinedar@gmail.com>
2018-12-19 10:51:43 -0500

    Adding some test for layers with same name and diferent geometries.

--- asistente_ladm_col/tests/test_get_layers.py
+++ asistente_ladm_col/tests/test_get_layers.py
@@ -1,9 +1,6 @@
-import datetime
-
 import nose2
-import psycopg2
-from qgis.core import QgsApplication, QgsProject
-from qgis.analysis import QgsNativeAlgorithms
+import itertools
+from qgis.core import QgsProject, QgsWkbTypes
 from processing.core.Processing import Processing
 from qgis.testing import (unittest,
                           start_app)
@@ -12,39 +9,29 @@ start_app() # need to start before asistente_ladm_col.tests.utils
 
 from asistente_ladm_col.tests.utils import (import_projectgenerator,
                                             get_dbconn,
-                                            get_test_path,
                                             restore_schema,
-                                            clean_table)
-
+                                            )
 
 from asistente_ladm_col.utils.qgis_utils import QGISUtils
 from asistente_ladm_col.config.table_mapping_config import BOUNDARY_POINT_TABLE, PLOT_TABLE
-from asistente_ladm_col.config.general_config import DEFAULT_EPSG
 
 import_projectgenerator()
+
 
 class TestGetLayers(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         Processing.initialize()
-        #QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
         print("\nINFO: Setting up copy CSV points to DB validation...")
         self.qgis_utils = QGISUtils()
         self.db_connection = get_dbconn('test_ladm_col')
-        #self.db_connection_3d = get_dbconn('test_ladm_col_3d')
         result = self.db_connection.test_connection()
         print('test_connection', result)
         if not result[1]:
             print('The test connection is not working')
             return
         restore_schema('test_ladm_col')
-        #restore_schema('test_ladm_col_3d')
-
-    # def check_necessary_domains_load(self, layer):
-    #     print("\nLoading layers...")
-    #     related_domains = self.qgis_utils.get_related_domains(layer, [])
-    #     return related_domains
 
     def test_get_layers(self):
         RELATED_TABLES_DOMAINS = {'puntolindero': ["col_acuerdotipo", "col_defpuntotipo",
@@ -58,9 +45,10 @@ class TestGetLayers(unittest.TestCase):
         print("\nINFO: Validating Functionality of get_layers...")
 
         self.qgis_utils.cache_layers_and_relations(self.db_connection)
-        print("---------------------->", self.qgis_utils._layers, self.qgis_utils._relations, self.qgis_utils._bags_of_enum)
         QgsProject.instance().clear()
         print("\nINFO: Validating when no exist loaded layers...")
+        # This test load puntolindero and terreno tables, check layers in layer tree after this
+        # and finish with comparision between loaded layers and needed layers.
         for layer in [BOUNDARY_POINT_TABLE, PLOT_TABLE]:
             loaded_tables = self.qgis_utils.get_layer(self.db_connection, layer, load=True)
             self.assertIn(loaded_tables.name(), layer)
@@ -70,53 +58,51 @@ class TestGetLayers(unittest.TestCase):
                 self.assertIn(layer_related, loaded_layers_tree_names)
             QgsProject.instance().clear()
 
+        print("\nINFO: Validating when exist loaded layers...")
 
+        print("Try this for {} layer".format(BOUNDARY_POINT_TABLE))
+        # preload some layers
+        for pre_load in ["col_acuerdotipo", "col_monumentaciontipo"]:
+            self.qgis_utils.get_layer(self.db_connection, pre_load, load=True)
+        # Load model layer
+        self.qgis_utils.get_layer(self.db_connection, BOUNDARY_POINT_TABLE, load=True)
+        # check number if element in Layer Tree and needed element are the same.
+        loaded_layers_tree_names = len(RELATED_TABLES_DOMAINS[BOUNDARY_POINT_TABLE])
+        layer_tree_elements = len([layer.name() for layer in self.qgis_utils.get_ladm_layers_from_layer_tree(self.db_connection)])
+        self.assertEqual(loaded_layers_tree_names, layer_tree_elements, "Same Element Numbers... OK")
+        # Load again preloaded layer for check not duplicate layers in load
+        for pre_load in ["col_acuerdotipo", "col_monumentaciontipo"]:
+            self.qgis_utils.get_layer(self.db_connection, pre_load, load=True)
+        layer_tree_elements = len([layer.name() for layer in self.qgis_utils.get_ladm_layers_from_layer_tree(self.db_connection)])
+        self.assertEqual(loaded_layers_tree_names, layer_tree_elements, "Function duplicate layers... Fail!!!")
+        QgsProject.instance().clear()
 
-        # self.qgis_utils.disable_automatic_fields(self.db_connection, BOUNDARY_POINT_TABLE)
-        # self.upload_points_from_csv()
-        # self.validate_points_in_db()
-        # clean_table('test_ladm_col', BOUNDARY_POINT_TABLE)
+        print("Try this for {} layer".format(PLOT_TABLE))
+        # preload some layers
+        for pre_load in ["la_nivel", "la_relacionsuperficietipo"]:
+            self.qgis_utils.get_layer(self.db_connection, pre_load, load=True)
+        # Load model layer
+        self.qgis_utils.get_layer(self.db_connection, PLOT_TABLE, geometry_type=QgsWkbTypes.Polygon, load=True)
+        # check number if element in Layer Tree and needed element are the same.
+        loaded_layers_tree_names = len(RELATED_TABLES_DOMAINS[PLOT_TABLE])
+        layer_tree_elements = len([layer.name() for layer in self.qgis_utils.get_ladm_layers_from_layer_tree(self.db_connection)])
+        self.assertEqual(loaded_layers_tree_names, layer_tree_elements, "Same Element Numbers... OK")
+        for pre_load in ["la_nivel", "la_relacionsuperficietipo"]:
+            self.qgis_utils.get_layer(self.db_connection, pre_load, load=True)
+        layer_tree_elements = len([layer.name() for layer in self.qgis_utils.get_ladm_layers_from_layer_tree(self.db_connection)])
+        self.assertEqual(loaded_layers_tree_names, layer_tree_elements, "Function duplicate layers... Fail!!!")
 
-    # def upload_points_from_csv(self):
-    #     print("Copying CSV data with no elevation...")
-    #     csv_path = get_test_path('csv/puntos_fixed.csv')
-    #     txt_delimiter = ';'
-    #     cbo_longitude = 'x'
-    #     cbo_latitude = 'y'
-    #     res = self.qgis_utils.copy_csv_to_db(csv_path,
-    #                                 txt_delimiter,
-    #                                 cbo_longitude,
-    #                                 cbo_latitude,
-    #                                 self.db_connection,
-    #                                 DEFAULT_EPSG,
-    #                                 BOUNDARY_POINT_TABLE)
-    #     self.assertEqual(res, True)
-    #
-    # def test_upload_points_from_csv_crs_wgs84(self):
-    #     print("\nINFO: Copying CSV data with EPSG:4326...")
-    #     clean_table('test_ladm_col', BOUNDARY_POINT_TABLE)
-    #     self.qgis_utils.disable_automatic_fields(self.db_connection, BOUNDARY_POINT_TABLE)
-    #     self.upload_points_from_csv_crs_wgs84()
-    #     self.validate_points_in_db_from_wgs84()
-    #     clean_table('test_ladm_col', BOUNDARY_POINT_TABLE)
-    #
-    # def upload_points_from_csv_crs_wgs84(self):
-    #     print("Copying CSV data in WGS84...")
-    #     csv_path = get_test_path('csv/puntos_crs_4326_wgs84.csv')
-    #     txt_delimiter = ';'
-    #     cbo_longitude = 'x'
-    #     cbo_latitude = 'y'
-    #     epsg =  '4326'
-    #
-    #     res = self.qgis_utils.copy_csv_to_db(csv_path,
-    #                                 txt_delimiter,
-    #                                 cbo_longitude,
-    #                                 cbo_latitude,
-    #                                 self.db_connection,
-    #                                 epsg,
-    #                                 BOUNDARY_POINT_TABLE)
-    #
-    #     self.assertEqual(res, True)
+        QgsProject.instance().clear()
+        print("\nINFO: Validating when loaded layers are the same name...")
+        # Load terreno without geometry parameter load point and polygon layer with different geometries (10 layers)
+        self.qgis_utils.get_layer(self.db_connection, PLOT_TABLE, load=True)
+        toc_layers = [l for l in self.qgis_utils.get_ladm_layers_from_layer_tree(self.db_connection)]
+        toc_names = [l.name() for l in toc_layers]
+        same_name_layers = [layer for layer in toc_layers if toc_names.count(layer.name()) > 1]
+        for layer_1, layer_2 in itertools.combinations(same_name_layers, 2):
+            if layer_1.name() == layer_2.name():
+                self.assertNotEqual(layer_1.geometryType(), layer_2.geometryType(), "Function get_layers loas Layers with same name and geometry... Fail!!!")
+
 
 if __name__ == '__main__':
     nose2.main()
