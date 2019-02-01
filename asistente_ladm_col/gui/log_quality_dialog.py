5194e32052030d5d152e40ef80a2eeb4fe22afe8
jhonalex06 <jhonsigpjc@gmail.com>
2019-02-01 10:47:36 -0500

    Add notification for the route of the report generated

--- asistente_ladm_col/gui/log_quality_dialog.py
+++ asistente_ladm_col/gui/log_quality_dialog.py
@@ -18,6 +18,7 @@
 """
 import os
 import time
+from qgis.core import Qgis
 from qgis.PyQt.QtWidgets import QDialog
 from qgis.PyQt.QtPrintSupport import QPrinter
 from qgis.PyQt.QtWidgets import (QFileDialog,
@@ -26,6 +27,7 @@ from qgis.PyQt.QtCore import (QCoreApplication,
                               QSettings)
 from ..utils import get_ui_class
 from asistente_ladm_col.utils.utils import set_time_format
+from ..utils.qt_utils import normalize_local_url
 
 LOG_DIALOG_UI = get_ui_class('dlg_log_quality.ui')
 
@@ -52,7 +54,7 @@ class LogQualityDialog(QDialog, LOG_DIALOG_UI):
                                                                                       'Export to PDF'),
                                                            settings.value(
                                                                'Asistente-LADM_COL/log_quality_dialog/save_path', '.'),
-                                                           filter="PDF (*.pdf)")
+                                                           filter="PDF (*.pdf)")               
 
         if new_filename:
             settings.setValue('Asistente-LADM_COL/log_quality_dialog/save_path', os.path.dirname(new_filename))
@@ -71,4 +73,8 @@ class LogQualityDialog(QDialog, LOG_DIALOG_UI):
             printer.setOutputFormat(QPrinter.PdfFormat)
             printer.setOutputFileName(new_filename)
             self.txt_log_quality.print(printer)
+
+            msg = QCoreApplication.translate("LogQualityDialog", 
+                "All Quality Check report successfully generated in folder <a href='file:///{path}'>{path}</a>!").format(path=normalize_local_url(new_filename))
+            self.qgis_utils.message_with_duration_emitted.emit(msg, Qgis.Success, 0) 
 
