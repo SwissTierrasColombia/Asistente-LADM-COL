3f406bc2a5169ca4f510c96a03819d0e7497a2c7
yesidpol <yesidpol.3@gmail.com>
2019-03-07 14:50:57 -0500

    add class metainfo

--- asistente_ladm_col/db_support/db_config_panel.py
+++ asistente_ladm_col/db_support/db_config_panel.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                              Asistente LADM_COL
+                             --------------------
+        begin                : 2019-02-21
+        git sha              : :%H$
+        copyright            : (C) 2019 by Yesid Polanía (BSF Swissphoto)
+        email                : yesidpol.3@gmail.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License v3.0 as          *
+ *   published by the Free Software Foundation.                            *
+ *                                                                         *
+ ***************************************************************************/
+"""
 from qgis.PyQt.QtCore import (QObject,pyqtSignal)
 from qgis.core import (Qgis)
 
