13fba0c1218de495cfa12df46affdcf2dbde36bb
Leo Cardona <leo.cardona.p@gmail.com>
2019-05-02 16:12:14 -0500

    Verify the required qgis version before starting the plugin

--- asistente_ladm_col/__init__.py
+++ asistente_ladm_col/__init__.py
@@ -20,21 +20,38 @@ from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsApplication,
                        Qgis)
 
-from .config.general_config import PLUGIN_NAME
+from .config.general_config import (PLUGIN_NAME,
+                                    QGIS_REQUIRED_VERSION,
+                                    QGIS_REQUIRED_VERSION_INT)
 
 
 def classFactory(iface):
-    try:
-        from .asistente_ladm_col_plugin import AsistenteLADMCOLPlugin
-    except ImportError as e:
-        iface.messageBar().pushMessage("Asistente LADM_COL",
-            QCoreApplication.translate("__init__",
-               "There was a problem loading the plugin {}. See the log for details.").format(PLUGIN_NAME),
-            1, 0)
+    if Qgis.QGIS_VERSION_INT >= QGIS_REQUIRED_VERSION_INT:
+        try:
+            from .asistente_ladm_col_plugin import AsistenteLADMCOLPlugin
+        except ImportError as e:
+            iface.messageBar().pushMessage("Asistente LADM_COL",
+                                           QCoreApplication.translate("__init__",
+                                                                      "There was a problem loading the plugin {}. See the log for details.").format(
+                                               PLUGIN_NAME),
+                                           1, 0)
 
-        QgsApplication.messageLog().logMessage("ERROR while loading the plugin: " + repr(e), PLUGIN_NAME, Qgis.Critical)
+            QgsApplication.messageLog().logMessage("ERROR while loading the plugin: " + repr(e), PLUGIN_NAME,
+                                                   Qgis.Critical)
+
+            from mock import Mock
+            return Mock()
+
+        return AsistenteLADMCOLPlugin(iface)
+    else:
+        iface.messageBar().pushMessage("Asistente LADM_COL",
+                                       QCoreApplication.translate("__init__",
+                                                                  "{} plugin requires QGIS {} version or higher. Please install the required version.").format(
+                                           PLUGIN_NAME,
+                                           QGIS_REQUIRED_VERSION),
+                                       1, 0)
+
+        QgsApplication.messageLog().logMessage("ERROR while loading the plugin: ", PLUGIN_NAME, Qgis.Critical)
 
         from mock import Mock
         return Mock()
-
-    return AsistenteLADMCOLPlugin(iface)
