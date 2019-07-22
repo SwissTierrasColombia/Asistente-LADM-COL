1a5637f36d0eb62c25a4d32f1ff7f59ff0996f9c
gacarrillor <gcarrillo@linuxmail.org>
2019-07-22 15:00:11 -0500

    [queries] Don't zoom when identifying plots, only for alphanumeric queries

--- asistente_ladm_col/gui/dockwidget_queries.py
+++ asistente_ladm_col/gui/dockwidget_queries.py
@@ -254,26 +254,33 @@ class DockWidgetQueries(QgsDockWidget, DOCKWIDGET_UI):
                                     duration=500)
 
         with OverrideCursor(Qt.WaitCursor):
-            self._plot_layer.selectByIds([plot_feature.id()])
             if not self.isVisible():
                 self.show()
 
-            self.search_data_by_component(plot_t_id=plot_t_id)
+            self.search_data_by_component(plot_t_id=plot_t_id, zoom_and_select=False)
+            self._plot_layer.selectByIds([plot_feature.id()])
 
     def search_data_by_component(self, **kwargs):
         self._plot_layer.removeSelection()
+
+        # Read zoom_and_select parameter and remove it from kwargs
+        bZoom = False
+        if 'zoom_and_select' in kwargs:
+            bZoom = kwargs['zoom_and_select']
+            del kwargs['zoom_and_select']
 
         records = self._db.get_igac_basic_info(**kwargs)
         self.tree_view_basic.setModel(TreeModel(data=records))
         self.tree_view_basic.expandAll()
 
-        # Zoom to resulting plots
-        plot_t_ids = self.get_plot_t_ids_from_basic_info(records)
-        if plot_t_ids:
-            features = self.ladm_data.get_features_from_t_ids(self._plot_layer, plot_t_ids, True, True)
-            plot_ids = [feature.id() for feature in features]
-            self.zoom_to_features_requested.emit(self._plot_layer, plot_ids, list(), 500)
-            self._plot_layer.selectByIds(plot_ids)
+        if bZoom:
+            # Zoom to resulting plots
+            plot_t_ids = self.get_plot_t_ids_from_basic_info(records)
+            if plot_t_ids:
+                features = self.ladm_data.get_features_from_t_ids(self._plot_layer, plot_t_ids, True, True)
+                plot_ids = [feature.id() for feature in features]
+                self.zoom_to_features_requested.emit(self._plot_layer, plot_ids, list(), 500)
+                self._plot_layer.selectByIds(plot_ids)
 
         records = self._db.get_igac_legal_info(**kwargs)
         self.tree_view_legal.setModel(TreeModel(data=records))
@@ -309,12 +316,11 @@ class DockWidgetQueries(QgsDockWidget, DOCKWIDGET_UI):
         query = self.txt_alphanumeric_query.value()
         if query:
             if option == FMI_FIELD:
-                self.search_data_by_component(parcel_fmi=query)
+                self.search_data_by_component(parcel_fmi=query, zoom_and_select=True)
             elif option == PARCEL_NUMBER_FIELD:
-                self.search_data_by_component(parcel_number=query)
+                self.search_data_by_component(parcel_number=query, zoom_and_select=True)
             else: # previous_parcel_number
-                self.search_data_by_component(previous_parcel_number=query)
-
+                self.search_data_by_component(previous_parcel_number=query, zoom_and_select=True)
         else:
             self.iface.messageBar().pushMessage("Asistente LADM_COL",
                 QCoreApplication.translate("DockWidgetQueries", "First enter a query"))
