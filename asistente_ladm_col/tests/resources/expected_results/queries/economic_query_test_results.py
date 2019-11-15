2a750786efb9c4528be4806db8d1ddc60061e0f5
Leo Cardona <leo.cardona.p@gmail.com>
2019-11-15 15:03:08 -0500

    fix tests get layers and queries

--- asistente_ladm_col/tests/resources/expected_results/queries/economic_query_test_results.py
+++ asistente_ladm_col/tests/resources/expected_results/queries/economic_query_test_results.py
@@ -1,84 +1,84 @@
 economic_query_test_results = {
     'query_by_plot_id': [
         {
-            "id": 8312,
             "attributes": {
-                "Avalúo terreno [COP]": 493429.8,
-                "Área de terreno [m2]": 4934.3,
                 "predio": [
                     {
-                        "id": 7897,
                         "attributes": {
-                            "Nombre": "El Pomarroso",
-                            "Departamento": "25",
-                            "Municipio": "394",
-                            "NUPRE": "0033",
-                            "FMI": None,
-                            "Número predial": "253940000000000230073000000000",
-                            "Número predial anterior": "25394000000230073000",
                             "Avalúo predio [COP]": None,
                             "Tipo": "Privado",
                             "Destinación económica": None,
+                            "Número predial anterior": "25394000000230073000",
+                            "Departamento": "25",
                             "op_construccion": [
                                 {
-                                    "id": 7536,
                                     "attributes": {
                                         "Avalúo [COP]": 2.1,
-                                        "Área construcción": 25.8,
                                         "op_unidadconstruccion": [
                                             {
-                                                "id": 8414,
                                                 "attributes": {
                                                     "Avalúo": 650248999.1,
-                                                    "Área construida [m2]": 25.8,
-                                                    "Área privada construida [m2]": 24.8,
-                                                    "Número de pisos": 2,
-                                                    "Ubicación en el piso": 4,
                                                     "Uso": "Oficinas o consultorios colonial",
-                                                    "Año construcción": 2015
-                                                }
+                                                    "Ubicación en el piso": 4,
+                                                    "Número de pisos": 2,
+                                                    "Año construcción": 2015,
+                                                    "Área privada construida [m2]": 24.8,
+                                                    "Área construida [m2]": 25.8
+                                                },
+                                                "id": 1524
                                             }
-                                        ]
-                                    }
+                                        ],
+                                        "Área construcción": 25.8
+                                    },
+                                    "id": 637
                                 },
                                 {
-                                    "id": 7539,
                                     "attributes": {
                                         "Avalúo [COP]": 0.5,
-                                        "Área construcción": 108.0,
                                         "op_unidadconstruccion": [
                                             {
-                                                "id": 8424,
                                                 "attributes": {
                                                     "Avalúo": 575417953.3,
-                                                    "Área construida [m2]": 19.0,
-                                                    "Área privada construida [m2]": 18.0,
-                                                    "Número de pisos": 12,
-                                                    "Ubicación en el piso": 0,
                                                     "Uso": "Comercio",
-                                                    "Año construcción": 1997
-                                                }
+                                                    "Ubicación en el piso": 0,
+                                                    "Número de pisos": 12,
+                                                    "Año construcción": 1997,
+                                                    "Área privada construida [m2]": 18,
+                                                    "Área construida [m2]": 19
+                                                },
+                                                "id": 1534
                                             },
                                             {
-                                                "id": 8435,
                                                 "attributes": {
                                                     "Avalúo": 738516768.3,
-                                                    "Área construida [m2]": 108.0,
-                                                    "Área privada construida [m2]": 107.0,
-                                                    "Número de pisos": 7,
-                                                    "Ubicación en el piso": 5,
                                                     "Uso": "Casa de elbas",
-                                                    "Año construcción": 2012
-                                                }
+                                                    "Ubicación en el piso": 5,
+                                                    "Número de pisos": 7,
+                                                    "Año construcción": 2012,
+                                                    "Área privada construida [m2]": 107,
+                                                    "Área construida [m2]": 108
+                                                },
+                                                "id": 1545
                                             }
-                                        ]
-                                    }
+                                        ],
+                                        "Área construcción": 108
+                                    },
+                                    "id": 640
                                 }
-                            ]
-                        }
+                            ],
+                            "FMI": None,
+                            "Nombre": "El Pomarroso",
+                            "Número predial": "253940000000000230073000000000",
+                            "Municipio": "394",
+                            "NUPRE": "0033"
+                        },
+                        "id": 998
                     }
-                ]
-            }
+                ],
+                "Avalúo terreno [COP]": 493429.8,
+                "Área de terreno [m2]": 4934.3
+            },
+            "id": 1416
         }
     ]
 }
--- asistente_ladm_col/tests/resources/expected_results/queries/property_record_card_query_test_results.py
+++ asistente_ladm_col/tests/resources/expected_results/queries/property_record_card_query_test_results.py
@@ -1,39 +1,39 @@
 property_record_card_query_test_results = {
     'query_by_plot_id': [
         {
-            "id": 8312,
             "attributes": {
-                "Área de op_terreno [m2]": 4934.3,
                 "op_predio": [
                     {
-                        "id": 7897,
                         "attributes": {
-                            "Nombre": "El Pomarroso",
-                            "Departamento": "25",
-                            "Municipio": "394",
-                            "NUPRE": "0033",
-                            "FMI": None,
-                            "Número predial": "253940000000000230073000000000",
-                            "Número predial anterior": "25394000000230073000",
-                            "Tipo": "Privado",
-                            "Corregimiento": None,
-                            "Localidad/Comuna": None,
-                            "Barrio/Vereda": None,
-                            "Formalidad": None,
-                            "Destinación económica": None,
-                            "Clase suelo": None,
-                            "Categoría suelo": None,
                             "Tiene FMI": None,
+                            "Tipo": "Privado",
+                            "Nombre del reconocedor": None,
                             "Fecha de inicio de tenencia": None,
+                            "Nombre": "El Pomarroso",
+                            "Número predial anterior": "25394000000230073000",
+                            "Corregimiento": None,
                             "Número predial del predio matriz": None,
                             "Observaciones": None,
+                            "Departamento": "25",
+                            "Barrio/Vereda": None,
+                            "NUPRE": "0033",
+                            "Contacto de visita": [],
+                            "FMI": None,
                             "Fecha de visita predial": None,
-                            "Nombre del reconocedor": None,
-                            "Contacto de visita": []
-                        }
+                            "Clase suelo": None,
+                            "Número predial": "253940000000000230073000000000",
+                            "Categoría suelo": None,
+                            "Localidad/Comuna": None,
+                            "Municipio": "394",
+                            "Formalidad": None,
+                            "Destinación económica": None
+                        },
+                        "id": 998
                     }
-                ]
-            }
+                ],
+                "Área de op_terreno [m2]": 4934.3
+            },
+            "id": 1416
         }
     ]
 }
