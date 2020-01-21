01759ddeeabdfa36ca475d1284f23129657f9da4
lacardonap <leo.cardona.p@gmail.com>
2020-01-21 09:55:31 -0500

    Add tests to check in names fields and tables for all models

--- asistente_ladm_col/tests/test_cadastral_form_model.py
+++ asistente_ladm_col/tests/test_cadastral_form_model.py
@@ -5,8 +5,9 @@ from qgis.testing import (start_app,
 
 start_app() # need to start before asistente_ladm_col.tests.utils
 
-from asistente_ladm_col.config.table_mapping_config import (Names,
-                                                            ILICODE,
+from asistente_ladm_col.tests.utils import (get_required_fields,
+                                            get_required_tables)
+from asistente_ladm_col.config.table_mapping_config import (ILICODE,
                                                             T_ID,
                                                             DESCRIPTION,
                                                             DISPLAY_NAME)
@@ -18,62 +19,70 @@ class TestCadastralFormModel(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         restore_schema('test_ladm_cadastral_form_model')
-        self.db_connection = get_dbconn('test_ladm_cadastral_form_model')
-        self.names = Names()
+        self.db_connection_pg = get_dbconn('test_ladm_cadastral_form_model')
 
-    def test_required_models(self):
+    def test_required_models_pg(self):
         print("\nINFO: Validate if the schema for cadastral form model...")
-        result = self.db_connection.test_connection()
+        result = self.db_connection_pg.test_connection()
         self.assertTrue(result[0], 'The test connection is not working')
+        self.check_required_models(self.db_connection_pg)
 
-        self.assertTrue(self.db_connection.supplies_model_exists())
-        self.assertTrue(self.db_connection.snr_data_model_exists())
-        self.assertTrue(self.db_connection.supplies_integration_model_exists())
-        self.assertTrue(self.db_connection.operation_model_exists())
-        self.assertTrue(self.db_connection.valuation_model_exists())
-        self.assertTrue(self.db_connection.cadastral_form_model_exists())
-        self.assertFalse(self.db_connection.ant_model_exists())
-        self.assertFalse(self.db_connection.reference_cartography_model_exists())
+    def check_required_models(self, db_connection):
+        self.assertTrue(db_connection.supplies_model_exists())
+        self.assertTrue(db_connection.snr_data_model_exists())
+        self.assertTrue(db_connection.supplies_integration_model_exists())
+        self.assertTrue(db_connection.operation_model_exists())
+        self.assertTrue(db_connection.valuation_model_exists())
+        self.assertTrue(db_connection.cadastral_form_model_exists())
+        self.assertFalse(db_connection.ant_model_exists())
+        self.assertFalse(db_connection.reference_cartography_model_exists())
 
-    def test_names_from_model(self):
+    def test_names_from_model_pg(self):
         print("\nINFO: Validate names for cadastral form model...")
-        result = self.db_connection.test_connection()
+        result = self.db_connection_pg.test_connection()
         self.assertTrue(result[0], 'The test connection is not working')
 
-        dict_names = self.db_connection.get_table_and_field_names()
+        dict_names = self.db_connection_pg.get_table_and_field_names()
         self.assertEqual(len(dict_names), 177)
-
         expected_dict = {T_ID: 't_id',
                          ILICODE: 'ilicode',
                          DESCRIPTION: 'description',
                          DISPLAY_NAME: 'dispname',
-                         'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM': {
-                             'table_name': 'fcm_formulario_unico_cm',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Barrio_Vereda': 'barrio_vereda',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Categoria_Suelo': 'categoria_suelo',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Clase_Suelo': 'clase_suelo',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Corregimiento': 'corregimiento',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Destinacion_Economica': 'destinacion_economica',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Fecha_Inicio_Tenencia': 'fecha_inicio_tenencia',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Fecha_Visita_predial': 'fecha_visita_predial',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Formalidad': 'formalidad',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Localidad_Comuna': 'localidad_comuna',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Nombre_Reconocedor': 'nombre_reconocedor',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Numero_Predial_Predio_Matriz': 'numero_predial_predio_matriz',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Observaciones': 'observaciones',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Tiene_FMI': 'tiene_fmi',
-                             'Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM.Numeros_Prediales_Englobe..Formulario_Catastro.Formulario_Catastro.FCM_Formulario_Unico_CM': 'fcm_formulario_unic_cm_numeros_prediales_englobe',
-                             'Formulario_Catastro.Formulario_Catastro.fcm_formulario_predio.op_predio..Operacion.Operacion.OP_Predio': 'op_predio'
+                         'Formulario_Catastro.FCM_DestinacionEconomicaTipo': {
+                             'table_name': 'fcm_destinacioneconomicatipo'
                          }}
 
         for k,v in expected_dict.items():
             self.assertIn(k, dict_names)
             self.assertEqual(v, dict_names[k])
 
+    def test_required_table_names_pg(self):
+        print("\nINFO: Validate minimum required tables from names...")
+        result = self.db_connection_pg.test_connection()
+        self.assertTrue(result[0], 'The test connection is not working')
+        self.check_required_table_names(self.db_connection_pg)
+
+    def check_required_table_names(self, db_connection):
+        test_required_tables = ['MORE_BFS_T', 'LESS_BFS_T', 'POINT_BFS_T', 'COL_POINT_SOURCE_T', 'COL_RRR_SOURCE_T', 'COL_UE_BAUNIT_T', 'COL_UE_SOURCE_T', 'COL_BAUNIT_SOURCE_T', 'COL_CCL_SOURCE_T', 'OP_BUILDING_TYPE_D', 'OP_DOMAIN_BUILDING_TYPE_D', 'OP_BUILDING_UNIT_TYPE_D', 'OP_GROUP_PARTY_T', 'OP_BUILDING_UNIT_T', 'OP_BUILDING_T', 'OP_RIGHT_T', 'OP_ADMINISTRATIVE_SOURCE_T', 'OP_SPATIAL_SOURCE_T', 'OP_PARTY_T', 'OP_BOUNDARY_T', 'OP_PARCEL_T', 'OP_BOUNDARY_POINT_T', 'OP_RESTRICTION_T', 'OP_RIGHT_OF_WAY_T', 'OP_PLOT_T', 'OP_ADMINISTRATIVE_SOURCE_TYPE_D', 'OP_PARTY_TYPE_D', 'OP_PARCEL_TYPE_D', 'OP_CONTROL_POINT_TYPE_D', 'OP_SURVEY_POINT_TYPE_D', 'OP_POINT_TYPE_D']
+        required_tables = get_required_tables(db_connection)
+        for test_required_table in test_required_tables:
+            self.assertIn(test_required_table, required_tables)
+
+    def test_required_field_names_pg(self):
+        print("\nINFO: Validate minimum required fields from names...")
+        result = self.db_connection_pg.test_connection()
+        self.assertTrue(result[0], 'The test connection is not working')
+        self.check_required_field_names(self.db_connection_pg)
+
+    def check_required_field_names(self, db_connection):
+        test_required_fields = ['EXT_ARCHIVE_S_DATA_F', 'FRACTION_S_NUMERATOR_F', 'FRACTION_S_OP_RIGHT_F', 'FRACTION_S_OP_RESTRICTION_F', 'MORE_BFS_T_OP_BOUNDARY_F', 'MORE_BFS_T_OP_BUILDING_F', 'MORE_BFS_T_OP_RIGHT_OF_WAY_F', 'MORE_BFS_T_OP_PLOT_F', 'MORE_BFS_T_OP_BUILDING_UNIT_F', 'LESS_BFS_T_OP_BOUNDARY_F', 'LESS_BFS_T_OP_BUILDING_F', 'LESS_BFS_T_OP_RIGHT_OF_WAY_F', 'LESS_BFS_T_OP_PLOT_F', 'LESS_BFS_T_OP_BUILDING_UNIT_F', 'FRACTION_S_MEMBER_F', 'MEMBERS_T_GROUP_PARTY_F', 'MEMBERS_T_PARTY_F', 'POINT_BFS_T_OP_BOUNDARY_F', 'POINT_BFS_T_OP_CONTROL_POINT_F', 'POINT_BFS_T_OP_SURVEY_POINT_F', 'POINT_BFS_T_OP_BOUNDARY_POINT_F', 'COL_POINT_SOURCE_T_SOURCE_F', 'COL_POINT_SOURCE_T_OP_CONTROL_POINT_F', 'COL_UE_BAUNIT_T_OP_BUILDING_F', 'COL_UE_BAUNIT_T_OP_BUILDING_UNIT_F', 'COL_UE_BAUNIT_T_OP_RIGHT_OF_WAY_F', 'COL_UE_SOURCE_T_SOURCE_F', 'COL_UE_SOURCE_T_OP_BUILDING_F', 'COL_UE_SOURCE_T_OP_RIGHT_OF_WAY_F', 'COL_UE_SOURCE_T_OP_PLOT_F', 'COL_UE_SOURCE_T_OP_BUILDING_UNIT_F', 'BAUNIT_SOURCE_T_SOURCE_F', 'BAUNIT_SOURCE_T_UNIT_F', 'COL_CCL_SOURCE_T_SOURCE_F', 'COL_CCL_SOURCE_T_BOUNDARY_F', 'COL_GROUP_PARTY_T_TYPE_F', 'COL_PARTY_T_NAME_F']
+        required_fields = get_required_fields(db_connection)
+        for test_required_field in test_required_fields:
+            self.assertIn(test_required_field, required_fields)
 
     @classmethod
     def tearDownClass(self):
-        self.db_connection.conn.close()
+        self.db_connection_pg.conn.close()
 
 
 if __name__ == '__main__':
--- asistente_ladm_col/tests/test_snr_data_model.py
+++ asistente_ladm_col/tests/test_snr_data_model.py
@@ -5,8 +5,9 @@ from qgis.testing import (start_app,
 
 start_app() # need to start before asistente_ladm_col.tests.utils
 
-from asistente_ladm_col.config.table_mapping_config import (Names,
-                                                            ILICODE,
+from asistente_ladm_col.tests.utils import (get_required_fields,
+                                            get_required_tables)
+from asistente_ladm_col.config.table_mapping_config import (ILICODE,
                                                             T_ID,
                                                             DESCRIPTION,
                                                             DISPLAY_NAME)
@@ -18,29 +19,31 @@ class TestSNRDataModel(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         restore_schema('test_ladm_snr_data')
-        self.db_connection = get_dbconn('test_ladm_snr_data')
-        self.names = Names()
+        self.db_connection_pg = get_dbconn('test_ladm_snr_data')
 
-    def test_required_models(self):
+    def test_required_models_pg(self):
         print("\nINFO: Validate if the schema for snr data model...")
-        result = self.db_connection.test_connection()
+        result = self.db_connection_pg.test_connection()
         self.assertTrue(result[0], 'The test connection is not working')
+        self.check_required_models(self.db_connection_pg)
 
-        self.assertFalse(self.db_connection.supplies_model_exists())
-        self.assertTrue(self.db_connection.snr_data_model_exists())
-        self.assertFalse(self.db_connection.supplies_integration_model_exists())
-        self.assertFalse(self.db_connection.operation_model_exists())
-        self.assertFalse(self.db_connection.valuation_model_exists())
-        self.assertFalse(self.db_connection.cadastral_form_model_exists())
-        self.assertFalse(self.db_connection.ant_model_exists())
-        self.assertFalse(self.db_connection.reference_cartography_model_exists())
+    def check_required_models(self, db_connection):
+        self.assertFalse(db_connection.supplies_model_exists())
+        self.assertTrue(db_connection.snr_data_model_exists())
+        self.assertFalse(db_connection.supplies_integration_model_exists())
+        self.assertFalse(db_connection.operation_model_exists())
+        self.assertFalse(db_connection.valuation_model_exists())
+        self.assertFalse(db_connection.cadastral_form_model_exists())
+        self.assertFalse(db_connection.ant_model_exists())
+        self.assertFalse(db_connection.reference_cartography_model_exists())
 
-    def test_names_from_model(self):
+    def test_names_from_db_pg(self):
         print("\nINFO: Validate names for SNR data model (small DB case)...")
-        result = self.db_connection.test_connection()
+        result = self.db_connection_pg.test_connection()
         self.assertTrue(result[0], 'The test connection is not working')
 
-        dict_names = self.db_connection.get_table_and_field_names()
+
+        dict_names = self.db_connection_pg.get_table_and_field_names()
         self.assertEqual(len(dict_names), 15)
 
         expected_dict = {T_ID: 't_id',
@@ -51,14 +54,39 @@ class TestSNRDataModel(unittest.TestCase):
                                                                      'Datos_SNR.Datos_SNR.snr_titular_derecho.Porcentaje_Participacion': 'porcentaje_participacion',
                                                                      'Datos_SNR.Datos_SNR.snr_titular_derecho.snr_derecho..Datos_SNR.Datos_SNR.SNR_Derecho': 'snr_derecho',
                                                                      'Datos_SNR.Datos_SNR.snr_titular_derecho.snr_titular..Datos_SNR.Datos_SNR.SNR_Titular': 'snr_titular'}}
-
         for k,v in expected_dict.items():
             self.assertIn(k, dict_names)
             self.assertEqual(v, dict_names[k])
 
+    def test_required_table_names_pg(self):
+        print("\nINFO: Validate minimum required tables from names...")
+        result = self.db_connection_pg.test_connection()
+        self.assertTrue(result[0], 'The test connection is not working')
+        self.check_required_table_names(self.db_connection_pg)
+
+    def check_required_table_names(self, db_connection):
+        test_required_tables = ['SNR_RIGHT_T', 'SNR_SOURCE_RIGHT_T', 'SNR_PARCEL_REGISTRY_T', 'SNR_TITLE_HOLDER_T', 'SNR_RIGHT_TYPE_D', 'SNR_TITLE_HOLDER_DOCUMENT_T', 'SNR_SOURCE_TYPE_D', 'SNR_TITLE_HOLDER_TYPE_D', 'EXT_ARCHIVE_S']
+        required_tables = get_required_tables(db_connection)
+
+        for test_required_table in test_required_tables:
+            self.assertIn(test_required_table, required_tables)
+
+    def test_required_field_names_pg(self):
+        print("\nINFO: Validate minimum required fields from names...")
+        result = self.db_connection_pg.test_connection()
+        self.assertTrue(result[0], 'The test connection is not working')
+        self.check_required_field_names(self.db_connection_pg)
+
+    def check_required_field_names(self, db_connection):
+        test_required_fields = ['EXT_ARCHIVE_S_DATA_F', 'EXT_ARCHIVE_S_EXTRACTION_F']
+        required_fields = get_required_fields(db_connection)
+
+        for test_required_field in test_required_fields:
+            self.assertIn(test_required_field, required_fields)
+
     @classmethod
     def tearDownClass(self):
-        self.db_connection.conn.close()
+        self.db_connection_pg.conn.close()
 
 
 if __name__ == '__main__':
