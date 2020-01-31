e548fac2cc39439516a8325a2886c55c50b8f149
lacardonap <leo.cardona.p@gmail.com>
2020-01-31 16:30:03 -0500

    [tests] Add test to check export data using Qgis Model Baker for GPKG. Fix export data test for PG

--- asistente_ladm_col/tests/test_qgis_model_baker.py
+++ asistente_ladm_col/tests/test_qgis_model_baker.py
@@ -1,6 +1,7 @@
 import nose2
 import os
 import tempfile
+from xml.dom.minidom import parse
 
 from qgis.testing import unittest, start_app
 start_app()
@@ -14,7 +15,6 @@ from QgisModelBaker.libili2db import (iliimporter,
                                       iliexporter)
 from QgisModelBaker.libili2db.globals import DbIliMode
 
-from asistente_ladm_col.utils.utils import md5sum
 from asistente_ladm_col.utils.qgis_utils import QGISUtils
 from asistente_ladm_col.config.general_config import (TOML_FILE_DIR,
                                                       DEFAULT_EPSG)
@@ -32,6 +32,7 @@ class TestQgisModelBaker(unittest.TestCase):
     def setUpClass(self):
         print("\nINFO: Setting up copy layer With different Geometries to DB validation...")
         self.qgis_utils = QGISUtils()
+        self.base_test_path = tempfile.mkdtemp()
 
     def test_export_data_in_pg(self):
         print("\nINFO: Validate Export Data in PG...")
@@ -64,8 +65,65 @@ class TestQgisModelBaker(unittest.TestCase):
         exporter.configuration = configuration
         exporter.configuration.xtffile = os.path.join(tempfile.mkdtemp(), 'test_export_data.xtf')
         self.assertEqual(exporter.run(), iliexporter.Exporter.SUCCESS)
-        self.assertEqual(md5sum(testdata_path('xtf/test_ladm_col_queries_v2.9.6.xtf')), md5sum(exporter.configuration.xtffile))
+        self.check_export_xtf(exporter.configuration.xtffile)
         db_pg.conn.close()
+
+    def test_export_data_in_gpkg(self):
+        print("\nINFO: Validate Export Data in GPKG...")
+        gpkg_path = get_test_copy_path('geopackage/test_export_data.gpkg')
+
+        base_config = BaseConfiguration()
+        base_config.custom_model_directories = testdata_path('models/LADM_COL')
+        base_config.custom_model_directories_enabled = True
+
+        configuration = ExportConfiguration()
+        configuration.base_configuration = base_config
+        configuration.ilimodels = ';'.join([LADMNames.SUPPORTED_LADM_MODEL,
+                                            LADMNames.SUPPORTED_SNR_DATA_MODEL,
+                                            LADMNames.SUPPORTED_SUPPLIES_MODEL,
+                                            LADMNames.SUPPORTED_SUPPLIES_INTEGRATION_MODEL,
+                                            LADMNames.SUPPORTED_OPERATION_MODEL,
+                                            LADMNames.SUPPORTED_ANT_MODEL,
+                                            LADMNames.SUPPORTED_CADASTRAL_FORM_MODEL,
+                                            LADMNames.SUPPORTED_VALUATION_MODEL])
+        configuration.dbfile = gpkg_path
+
+        exporter = iliexporter.Exporter()
+        exporter.tool = DbIliMode.ili2gpkg
+        exporter.configuration = configuration
+        exporter.configuration.xtffile = os.path.join(self.base_test_path, 'test_export_data.xtf')
+        self.assertEqual(exporter.run(), iliexporter.Exporter.SUCCESS)
+        self.check_export_xtf(exporter.configuration.xtffile)
+
+    def check_export_xtf(self, xtf_path):
+        test_xtf_dom = parse(testdata_path('xtf/test_ladm_col_queries_v2.9.6.xtf'))
+        test_xtf_op_building_count = len(test_xtf_dom.getElementsByTagName('Operacion_V2_9_6.Operacion.OP_Construccion'))
+        test_xtf_op_admin_source_count = len(test_xtf_dom.getElementsByTagName('Operacion_V2_9_6.Operacion.OP_FuenteAdministrativa'))
+        test_xtf_snr = test_xtf_dom.getElementsByTagName('Datos_SNR_V2_9_6.Datos_SNR')[0]
+        test_xtf_gc = test_xtf_dom.getElementsByTagName('Datos_Gestor_Catastral_V2_9_6.Datos_Gestor_Catastral')[0]
+        test_xtf_integration = test_xtf_dom.getElementsByTagName('Datos_Integracion_Insumos_V2_9_6.Datos_Integracion_Insumos')[0]
+        test_xtf_ant = test_xtf_dom.getElementsByTagName('ANT_V2_9_6.Fiso')[0]
+        test_xtf_cadastral_form = test_xtf_dom.getElementsByTagName('Formulario_Catastro_V2_9_6.Formulario_Catastro')[0]
+        test_xtf_valuation = test_xtf_dom.getElementsByTagName('Avaluos_V2_9_6.Avaluos')[0]
+
+        xtf_dom = parse(testdata_path(xtf_path))
+        xtf_op_building_count = len(xtf_dom.getElementsByTagName('Operacion_V2_9_6.Operacion.OP_Construccion'))
+        xtf_op_admin_source_count = len(xtf_dom.getElementsByTagName('Operacion_V2_9_6.Operacion.OP_FuenteAdministrativa'))
+        xtf_snr = xtf_dom.getElementsByTagName('Datos_SNR_V2_9_6.Datos_SNR')[0]
+        xtf_gc = xtf_dom.getElementsByTagName('Datos_Gestor_Catastral_V2_9_6.Datos_Gestor_Catastral')[0]
+        xtf_integration = xtf_dom.getElementsByTagName('Datos_Integracion_Insumos_V2_9_6.Datos_Integracion_Insumos')[0]
+        xtf_ant = xtf_dom.getElementsByTagName('ANT_V2_9_6.Fiso')[0]
+        xtf_cadastral_form = xtf_dom.getElementsByTagName('Formulario_Catastro_V2_9_6.Formulario_Catastro')[0]
+        xtf_valuation = xtf_dom.getElementsByTagName('Avaluos_V2_9_6.Avaluos')[0]
+
+        self.assertEqual(xtf_op_building_count, test_xtf_op_building_count)
+        self.assertEqual(xtf_op_admin_source_count, test_xtf_op_admin_source_count)
+        self.assertEqual(xtf_snr.toxml(), test_xtf_snr.toxml())
+        self.assertEqual(xtf_gc.toxml(), test_xtf_gc.toxml())
+        self.assertEqual(xtf_integration.toxml(), test_xtf_integration.toxml())
+        self.assertEqual(xtf_ant.toxml(), test_xtf_ant.toxml())
+        self.assertEqual(xtf_cadastral_form.toxml(), test_xtf_cadastral_form.toxml())
+        self.assertEqual(xtf_valuation.toxml(), test_xtf_valuation.toxml())
 
     def test_import_data_in_pg(self):
         print("\nINFO: Validate Import Data in PG...")
@@ -223,7 +281,7 @@ class TestQgisModelBaker(unittest.TestCase):
         configuration = SchemaImportConfiguration()
         configuration.base_configuration = base_config
         configuration.tool = DbIliMode.ili2gpkg
-        configuration.dbfile = os.path.join(tempfile.mkdtemp(), 'tmp_import_schema.gpkg')
+        configuration.dbfile = os.path.join(self.base_test_path, 'tmp_import_schema.gpkg')
         configuration.tomlfile = TOML_FILE_DIR
         configuration.epsg = DEFAULT_EPSG
         configuration.inheritance = LADMNames.DEFAULT_INHERITANCE
