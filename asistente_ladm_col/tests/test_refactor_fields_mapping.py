5c8f0ecbe32dda6dd6feafbf43c32c8f916464a0
lacardonap <leo.cardona.p@gmail.com>
2020-03-11 10:34:31 -0500

    [test] check refactor fields for some layers

--- asistente_ladm_col/tests/test_refactor_fields_mapping.py
+++ asistente_ladm_col/tests/test_refactor_fields_mapping.py
@@ -25,25 +25,49 @@ class TestRefactorFieldsMapping(unittest.TestCase):
         cls.db_gpkg_test = get_copy_gpkg_conn('test_export_data_qpkg')
         cls.qgis_utils = QGISUtils()
 
-    def test_refactor_field_for_boundary(self):
+        result_empty = cls.db_gpkg_empty.test_connection()
+        result_test = cls.db_gpkg_test.test_connection()
+        cls.assertTrue(result_empty[0], 'The test connection is not working for empty db')
+        cls.assertTrue(result_test[0], 'The test connection is not working for test data db')
+
+    def test_refactor_field(self):
         print('\nINFO: Validating refactor field for boundary layer ...')
-        result_empty = self.db_gpkg_empty.test_connection()
-        result_test = self.db_gpkg_test.test_connection()
-        self.assertTrue(result_empty[0], 'The test connection is not working for empty db')
-        self.assertTrue(result_test[0], 'The test connection is not working for test data db')
-        self.assertIsNotNone(self.db_gpkg_empty.names.OP_BOUNDARY_T, 'Names is None')
 
-        boundary_layer = self.qgis_utils.get_layer(self.db_gpkg_test, self.db_gpkg_test.names.OP_BOUNDARY_T, load=True)
-        test_layer = self.qgis_utils.get_layer(self.db_gpkg_empty, self.db_gpkg_empty.names.OP_BOUNDARY_T, load=True)
-        self.assertEqual(boundary_layer.featureCount(), 154)
-        self.assertEqual(test_layer.featureCount(), 0)
+        dict_layers_to_check = {
+            self.db_gpkg_test.names.OP_BOUNDARY_T: self.db_gpkg_empty.names.OP_BOUNDARY_T,
+            self.db_gpkg_test.names.OP_PLOT_T: self.db_gpkg_empty.names.OP_PLOT_T,
+            self.db_gpkg_test.names.OP_PARCEL_T: self.db_gpkg_empty.names.OP_PARCEL_T,
+            self.db_gpkg_test.names.OP_BOUNDARY_POINT_T: self.db_gpkg_empty.names.OP_BOUNDARY_POINT_T,
+            self.db_gpkg_test.names.OP_CONTROL_POINT_T: self.db_gpkg_empty.names.OP_CONTROL_POINT_T,
+            self.db_gpkg_test.names.OP_SURVEY_POINT_T: self.db_gpkg_empty.names.OP_SURVEY_POINT_T,
+            self.db_gpkg_test.names.OP_PARTY_T: self.db_gpkg_empty.names.OP_PARTY_T,
+            self.db_gpkg_test.names.OP_ADMINISTRATIVE_SOURCE_T: self.db_gpkg_empty.names.OP_ADMINISTRATIVE_SOURCE_T,
+            self.db_gpkg_test.names.OP_BUILDING_T: self.db_gpkg_empty.names.OP_BUILDING_T,
+            self.db_gpkg_test.names.OP_BUILDING_UNIT_T: self.db_gpkg_empty.names.OP_BUILDING_UNIT_T
+        }
 
-        print("Import data from test db to empty db for boundary using refactor fields")
-        run_etl_model(self.db_gpkg_empty.names, boundary_layer, test_layer, self.db_gpkg_empty.names.OP_BOUNDARY_POINT_T)
-        self.assertEqual(test_layer.featureCount(), 154)
+        feature_count_test = {
+            self.db_gpkg_test.names.OP_BOUNDARY_T: 154,
+            self.db_gpkg_test.names.OP_PLOT_T: 52,
+            self.db_gpkg_test.names.OP_PARCEL_T: 51,
+            self.db_gpkg_test.names.OP_BOUNDARY_POINT_T: 390,
+            self.db_gpkg_test.names.OP_CONTROL_POINT_T: 0,
+            self.db_gpkg_test.names.OP_SURVEY_POINT_T: 53,
+            self.db_gpkg_test.names.OP_PARTY_T: 36,
+            self.db_gpkg_test.names.OP_ADMINISTRATIVE_SOURCE_T: 50,
+            self.db_gpkg_test.names.OP_BUILDING_T: 17,
+            self.db_gpkg_test.names.OP_BUILDING_UNIT_T: 29
+        }
 
-        delete_features(test_layer)
-        self.assertEqual(test_layer.featureCount(), 0)
+        for layer_name_test, layer_name_empty in dict_layers_to_check.items():
+            layer = self.qgis_utils.get_layer(self.db_gpkg_test, layer_name_test, load=True)
+            test_layer = self.qgis_utils.get_layer(self.db_gpkg_empty, layer_name_empty, load=True)
+            self.assertEqual(layer.featureCount(), feature_count_test[layer_name_test], 'Error in {}'.format(layer_name_test))
+            self.assertEqual(test_layer.featureCount(), 0, 'Error in {}'.format(layer_name_test))
+
+            print("Import data from test db to empty db for {} using refactor fields".format(layer_name_test))
+            run_etl_model(self.db_gpkg_empty.names, layer, test_layer, layer_name_empty)
+            self.assertEqual(test_layer.featureCount(), feature_count_test[layer_name_test], 'Error in {}'.format(layer_name_test))
 
     @classmethod
     def tearDownClass(cls):
