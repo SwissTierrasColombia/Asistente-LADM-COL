ee380266a9668b62be3e52117107b1c82b661235
gacarrillor <gcarrillo@linuxmail.org>
2020-04-16 17:33:33 -0500

    A couple of minor fixes

--- asistente_ladm_col/lib/transitional_system/st_session/st_session.py
+++ asistente_ladm_col/lib/transitional_system/st_session/st_session.py
@@ -86,8 +86,8 @@ class STSession(QObject, metaclass=SingletonQObject):
             elif response.status_code == 500:
                 msg = QCoreApplication.translate("STSession", "There is an error in the login server!")
             elif response.status_code > 500 and response.status_code < 600:
-                msg = self.st_config.ST_STATUS_GT_500_MSG
-                self.logger.warning(__name__, self.st_config.ST_STATUS_GT_500_MSG)
+                msg = st_config.ST_STATUS_GT_500_MSG
+                self.logger.warning(__name__, st_config.ST_STATUS_GT_500_MSG)
             elif response.status_code == 401:
                 msg = QCoreApplication.translate("STSession", "Unauthorized client! The server won't allow requests from this client.")
             self.logger.warning(__name__, msg)
