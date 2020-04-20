ca3230b1f6d65cc1061d5418a7bc62d1e3cf546f
gacarrillor <gcarrillo@linuxmail.org>
2020-04-20 16:53:21 -0500

    [st] ST dock widget is not closable anymore, you need to logout to close it

--- asistente_ladm_col/gui/dialogs/dlg_settings.py
+++ asistente_ladm_col/gui/dialogs/dlg_settings.py
@@ -258,7 +258,7 @@ class SettingsDialog(QDialog, DIALOG_UI):
                 reply = QMessageBox.question(None,
                                              QCoreApplication.translate("SettingsDialog", "Warning?"),
                                              QCoreApplication.translate("SettingsDialog",
-                                                                        "You have a ST connection opened and you want to change your role.\nIf you confirm that you want to change your role, you'll be logged out from the ST.\nDo you really want to change your role?"),
+                                                                        "You have a ST connection opened and you want to change your role.\nIf you confirm that you want to change your role, you'll be logged out from the ST.\n\nDo you really want to change your role?"),
                                              QMessageBox.Yes | QMessageBox.Cancel, QMessageBox.Cancel)
                 if reply == QMessageBox.Yes:
                     STSession().logout()
