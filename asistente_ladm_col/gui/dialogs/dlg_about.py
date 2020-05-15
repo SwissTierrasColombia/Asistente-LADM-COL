4e6f598b3b6d40945c18eaf5d2a621347a87f4f4
lacardonap <leo.cardona.p@gmail.com>
2020-05-14 19:12:38 -0500

    download documentation in one language only

--- asistente_ladm_col/gui/dialogs/dlg_about.py
+++ asistente_ladm_col/gui/dialogs/dlg_about.py
@@ -126,7 +126,7 @@ class AboutDialog(QDialog, DIALOG_UI):
     def download_help(self):
         if is_connected(TEST_SERVER):
             self.btn_download_help.setEnabled(False)
-            url = '/'.join([HELP_DOWNLOAD, PLUGIN_VERSION, 'asistente_ladm_col_docs_{lang}.zip'.format(lang=QGIS_LANG)])
+            url = '/'.join([HELP_DOWNLOAD, PLUGIN_VERSION, 'asistente_ladm_col_docs.zip'])
             fetcher_task = QgsNetworkContentFetcherTask(QUrl(url))
             fetcher_task.taskCompleted.connect(self.enable_download_button)
             fetcher_task.fetched.connect(partial(self.save_file, fetcher_task))
