7623b716cda23a1f5531e8c8abc38ee2e6736b03
yesidpol <yesidpol.3@gmail.com>
2020-07-06 16:21:38 -0500

    [mssql] Add odbc drivers verification

--- asistente_ladm_col/config/config_db_supported.py
+++ asistente_ladm_col/config/config_db_supported.py
@@ -4,7 +4,8 @@ from asistente_ladm_col.lib.logger import Logger
 from asistente_ladm_col.lib.db.pg_factory import PGFactory
 from asistente_ladm_col.lib.db.gpkg_factory import GPKGFactory
 from asistente_ladm_col.utils.mssql_utils import (is_libqt5sql5_odbc_available,
-                                                  is_pyodbc_available)
+                                                  is_pyodbc_available,
+                                                  check_if_odbc_exists)
 
 from qgis.PyQt.QtCore import QCoreApplication
 
@@ -27,8 +28,9 @@ class ConfigDBsSupported(QObject):
 
         pyodbc_installed = is_pyodbc_available()
         libqt5sql5_odbc_installed = is_libqt5sql5_odbc_available()
+        driver_odbc_available = check_if_odbc_exists()
 
-        if pyodbc_installed and libqt5sql5_odbc_installed:
+        if pyodbc_installed and libqt5sql5_odbc_installed and driver_odbc_available:
             from asistente_ladm_col.lib.db.mssql_factory import MSSQLFactory
             db_factory = MSSQLFactory()
             self._db_factories[db_factory.get_id()] = db_factory
@@ -39,6 +41,10 @@ class ConfigDBsSupported(QObject):
         if not libqt5sql5_odbc_installed:
             self.logger.warning(__name__, QCoreApplication.translate("ConfigDBsSupported",
                                                            "MS SQL Server could not be configured. Library 'libqt5sql5-odbc' is missing!"))
+
+        if not driver_odbc_available:
+            self.logger.warning(__name__, QCoreApplication.translate("ConfigDBsSupported",
+                                                                     "MS SQL Server could not be configured. There is not any odbc driver installed"))
 
     def get_db_factories(self):
         return self._db_factories
--- asistente_ladm_col/utils/mssql_utils.py
+++ asistente_ladm_col/utils/mssql_utils.py
@@ -1,3 +1,5 @@
+import re
+
 
 def is_libqt5sql5_odbc_available():
     from qgis.PyQt.QtSql import QSqlDatabase
@@ -12,4 +14,31 @@ def is_pyodbc_available():
         result = True
     except ModuleNotFoundError:
         pass
+    return result
+
+
+def check_if_odbc_exists():
+    result = False
+
+    try:
+        odbc_drivers = get_odbc_drivers()
+        result = len(odbc_drivers) != 0
+    except ModuleNotFoundError:
+        pass
+    return result
+
+
+def get_odbc_drivers():
+    # Borrowed from Model Baker
+    import pyodbc
+    result = list()
+    regex_list = ['sql.*server', 'mssql', 'FreeTDS']
+    for item in pyodbc.drivers():
+        regex_sql_server = "({})".format("|".join(regex_list))
+
+        x = re.search(regex_sql_server, item, re.IGNORECASE)
+
+        if x:
+            result.append(item)
+
     return result
