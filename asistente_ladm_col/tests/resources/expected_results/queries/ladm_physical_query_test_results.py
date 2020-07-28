17009ca94f1fb98eed431a59c9ef318f92a0a19d
lacardonap <leo.cardona.p@gmail.com>
2020-07-28 11:10:36 -0500

    [tests] the tests associated with the sql queries are updated to validate the stored topology

--- asistente_ladm_col/tests/resources/expected_results/queries/ladm_physical_query_test_results.py
+++ asistente_ladm_col/tests/resources/expected_results/queries/ladm_physical_query_test_results.py
@@ -1,12 +1,12 @@
 expected_result_ladm_physical_query = {
     "lc_terreno": [
         {
-            "id": 1432,
+            "id": 1430,
             "attributes": {
                 "Área [m2]": 4934.3,
                 "lc_predio": [
                     {
-                        "id": 1007,
+                        "id": 1005,
                         "attributes": {
                             "Nombre": "El Pomarroso",
                             "NUPRE": "0033",
@@ -16,13 +16,13 @@ expected_result_ladm_physical_query = {
                             "Número predial anterior": "25394000000230073000",
                             "lc_construccion": [
                                 {
-                                    "id": 595,
+                                    "id": 593,
                                     "attributes": {
                                         "Área construcción": 108.0,
                                         "Número de pisos": 3,
                                         "lc_unidadconstruccion": [
                                             {
-                                                "id": 1541,
+                                                "id": 1539,
                                                 "attributes": {
                                                     "Número de pisos": 12,
                                                     "Uso": "(Comercial) Comercio",
@@ -36,7 +36,7 @@ expected_result_ladm_physical_query = {
                                                 }
                                             },
                                             {
-                                                "id": 1544,
+                                                "id": 1542,
                                                 "attributes": {
                                                     "Número de pisos": 7,
                                                     "Uso": "(Residencial) Casa Elbas",
@@ -56,13 +56,13 @@ expected_result_ladm_physical_query = {
                                     }
                                 },
                                 {
-                                    "id": 610,
+                                    "id": 608,
                                     "attributes": {
                                         "Área construcción": 25.8,
                                         "Número de pisos": 10,
                                         "lc_unidadconstruccion": [
                                             {
-                                                "id": 1531,
+                                                "id": 1529,
                                                 "attributes": {
                                                     "Número de pisos": 2,
                                                     "Uso": "(Comercial) Oficinas - Consultorios Coloniales",
@@ -86,10 +86,42 @@ expected_result_ladm_physical_query = {
                     }
                 ],
                 "lc_lindero externos": [
-
+                    {
+                        "id": 862,
+                        "attributes": {
+                            "Longitud [m]": 163.6
+                        }
+                    }
                 ],
                 "lc_puntolindero externos": [
-
+                    {
+                        "id": 1084,
+                        "attributes": {
+                            "Nombre": "3",
+                            "Coordenadas": "4844104.276 2143235.381 0"
+                        }
+                    },
+                    {
+                        "id": 1093,
+                        "attributes": {
+                            "Nombre": "3",
+                            "Coordenadas": "4844070.422 2143291.545 0"
+                        }
+                    },
+                    {
+                        "id": 1117,
+                        "attributes": {
+                            "Nombre": "3",
+                            "Coordenadas": "4844165.906 2143254.954 0"
+                        }
+                    },
+                    {
+                        "id": 1118,
+                        "attributes": {
+                            "Nombre": "3",
+                            "Coordenadas": "4844120.027 2143332.595 0"
+                        }
+                    }
                 ],
                 "lc_lindero internos": [
 
@@ -99,55 +131,55 @@ expected_result_ladm_physical_query = {
                 ],
                 "lc_puntolevantamiento": [
                     {
-                        "id": 1475,
+                        "id": 1473,
                         "attributes": {
                             "Coordenadas": "4844103.214 2143281.324 0"
                         }
                     },
                     {
-                        "id": 1476,
+                        "id": 1474,
                         "attributes": {
                             "Coordenadas": "4844111.106 2143273.872 0"
                         }
                     },
                     {
-                        "id": 1481,
+                        "id": 1479,
                         "attributes": {
                             "Coordenadas": "4844095.326 2143282.453 0"
                         }
                     },
                     {
-                        "id": 1482,
+                        "id": 1480,
                         "attributes": {
                             "Coordenadas": "4844097.419 2143285.417 0"
                         }
                     },
                     {
-                        "id": 1510,
+                        "id": 1508,
                         "attributes": {
                             "Coordenadas": "4844101.121 2143278.36 0"
                         }
                     },
                     {
-                        "id": 1515,
+                        "id": 1513,
                         "attributes": {
                             "Coordenadas": "4844138.286 2143263.089 0"
                         }
                     },
                     {
-                        "id": 1516,
+                        "id": 1514,
                         "attributes": {
                             "Coordenadas": "4844144.513 2143270.821 0"
                         }
                     },
                     {
-                        "id": 1519,
+                        "id": 1517,
                         "attributes": {
                             "Coordenadas": "4844132.908 2143275.323 0"
                         }
                     },
                     {
-                        "id": 1520,
+                        "id": 1518,
                         "attributes": {
                             "Coordenadas": "4844128.208 2143267.793 0"
                         }
@@ -155,7 +187,7 @@ expected_result_ladm_physical_query = {
                 ],
                 "lc_fuenteespacial": [
                     {
-                        "id": 713,
+                        "id": 711,
                         "attributes": {
                             "Tipo de fuente espacial": "Datos crudos (GPS, Estación total, LiDAR, etc.)",
                             "Estado disponibilidad": "Disponible",
@@ -165,7 +197,7 @@ expected_result_ladm_physical_query = {
                         }
                     },
                     {
-                        "id": 715,
+                        "id": 713,
                         "attributes": {
                             "Tipo de fuente espacial": "Datos crudos (GPS, Estación total, LiDAR, etc.)",
                             "Estado disponibilidad": "Disponible",
