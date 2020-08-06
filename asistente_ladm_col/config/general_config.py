8e615a66b41fcc0dfd4527e86dc7153cd24e16f4
lacardonap <leo.cardona.p@gmail.com>
2020-08-06 11:34:22 -0500

    Report library updated to version 0.8

--- asistente_ladm_col/config/general_config.py
+++ asistente_ladm_col/config/general_config.py
@@ -70,9 +70,9 @@ CYPTO_MD5SUM = 'a42e671dcc78f519020a16f4c47da588'
 # Version to be installed when creating reports (annex 17 - ANT Map)
 # (Other versions, if found, will be dropped in favor of this one)
 DEPENDENCY_REPORTS_DIR_NAME = os.path.join(DEPENDENCIES_BASE_PATH, 'impresion')
-REPORTS_REQUIRED_VERSION = '0.7'
+REPORTS_REQUIRED_VERSION = '0.8'
 URL_REPORTS_LIBRARIES = 'https://github.com/SwissTierrasColombia/LADM-COL_Reports/releases/download/{}/impresion.zip'.format(REPORTS_REQUIRED_VERSION)
-REPORTS_LIBRARIES_MD5SUM = '684fc2a5de34fa5986f2325e9e8ff435'
+REPORTS_LIBRARIES_MD5SUM = 'ebccc3bd2e4a9ccaf1da7b94dd222347'
 
 MODULE_HELP_MAPPING = {
     '' : 'index.html', # default module is '', just go to index.html
