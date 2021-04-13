cecebc3bc11c19878310851378d6daa89a8159b3
lacardonap <leo.cardona.p@gmail.com>
2021-04-13 08:25:31 -0500

    [quality] Spatial index created to improve performance of some quality rules

--- asistente_ladm_col/logic/quality/point_quality_rules.py
+++ asistente_ladm_col/logic/quality/point_quality_rules.py
@@ -214,6 +214,7 @@ class PointQualityRules:
                 fs.append(f)
         del filter_fs
         boundary_nodes_layer.dataProvider().addFeatures(fs)
+        GeometryUtils.create_spatial_index(boundary_nodes_layer)  # spatial index is created for better performance
 
         # Spatial Join between boundary_points and boundary_nodes
         spatial_join_layer = processing.run("native:joinattributesbylocation",
