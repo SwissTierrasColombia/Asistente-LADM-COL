a3317789a37a094d40bed5637b480d95e3d12154
Germán Carrillo <gcarrillo@linuxmail.org>
2021-10-26 17:20:06 -0500

    [core] Introduce XTFModelConverterRegistry; register a dummy converter to be shown in the converter dialog

---
+++ asistente_ladm_col/core/xtf_model_converter/xtf_model_converter_registry.py
@@ -0,0 +1,90 @@
+"""
+/***************************************************************************
+                              Asistente LADM-COL
+                             --------------------
+        begin           : 2021-10-26
+        git sha         : :%H$
+        copyright       : (C) 2021 by Germán Carrillo (SwissTierras Colombia)
+        email           : gcarrillo@linuxmail.org
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License v3.0 as          *
+ *   published by the Free Software Foundation.                            *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from asistente_ladm_col.app_interface import AppInterface
+from asistente_ladm_col.core.xtf_model_converter.ladm_col_model_converter import LADMColModelConverter
+from asistente_ladm_col.lib.logger import Logger
+from asistente_ladm_col.logic.xtf_model_converter.lev_cat_1_0_to_1_1_converter import Survey10To11Converter
+from asistente_ladm_col.utils.singleton import Singleton
+
+
+class XTFModelConverterRegistry(metaclass=Singleton):
+    """
+    Registry of supported model converters.
+    """
+    def __init__(self):
+        self.logger = Logger()
+        self.app = AppInterface()
+        self.__converters = dict()  # {converter_key1: LADMColModelConverter1, ...}
+
+        # Register default models
+        self.register_model_converter(Survey10To11Converter())
+
+    def register_model_converter(self, converter):
+        """
+        :param converter: LADMColModelConverter instance.
+        :return: True if the converter was registered, False otherwise.
+        """
+        if not isinstance(converter, LADMColModelConverter):
+            self.logger.warning(__name__, "The converter '{}' is not a 'LADMColModelConverter' instance!".format(converter.id()))
+            return False
+
+        if not converter.is_valid():
+            self.logger.warning(__name__, "The converter '{}' is not valid! Check the converter definition!".format(converter.id()))
+            return False
+
+        if converter.id() in self.__converters:
+            self.logger.warning(__name__, "The converter '{}' is already registered.".format(converter.id()))
+            return False
+
+        self.__converters[converter.id()] = converter
+        self.logger.info(__name__, "Model converter '{}' has been registered!".format(converter.id()))
+
+        return True
+
+    def unregister_model_converter(self, converter_key):
+        """
+        Unregisters a model converter.
+
+        :param converter_key: Id of the converter to unregister.
+        :return: True if the converter was unregistered, False otherwise.
+        """
+        if converter_key not in self.__converters:
+            self.logger.error(__name__, "Converter '{}' was not found in registered model converters, therefore, it cannot be unregistered!".format(converter_key))
+            return False
+
+        self.__converters[converter_key] = None
+        del self.__converters[converter_key]
+        self.logger.info(__name__, "Model converter '{}' has been unregistered!".format(converter_key))
+
+        return True
+
+    def get_converter(self, converter_key):
+        converter = self.__converters.get(converter_key, None)  # To avoid exceptions
+        if not converter:
+            self.logger.critical(__name__, "No model converter found with key '{}'".format(converter_key))
+
+        return converter
+
+    def get_converters_for_models(self, models):
+        converters = dict()  # {converter_key_1: converter_display_name_1, ...]
+        for converter_key, converter in self.__converters.items():
+            for model in models:
+                if converter.supports_source_model(model):
+                    converters[converter_key] = converter.display_name()
+
+        return converters
--- asistente_ladm_col/gui/xtf_model_converter/dlg_xtf_model_converter.py
+++ asistente_ladm_col/gui/xtf_model_converter/dlg_xtf_model_converter.py
@@ -33,9 +33,9 @@ from asistente_ladm_col.app_interface import AppInterface
 from asistente_ladm_col.lib.logger import Logger
 from asistente_ladm_col.utils.qt_utils import (FileValidator,
                                                Validators,
-                                               make_file_selector)
+                                               make_file_selector,
+                                               ProcessWithStatus)
 from asistente_ladm_col.utils import get_ui_class
-from asistente_ladm_col.lib.processing.custom_processing_feedback import CustomFeedback
 
 DIALOG_XTF_MODEL_CONVERTER_UI = get_ui_class('xtf_model_converter/dlg_xtf_model_converter.ui')
 
@@ -43,9 +43,10 @@ DIALOG_XTF_MODEL_CONVERTER_UI = get_ui_class('xtf_model_converter/dlg_xtf_model_
 class XTFModelConverterDialog(QDialog, DIALOG_XTF_MODEL_CONVERTER_UI):
     on_result = pyqtSignal(bool)  # whether the tool was run successfully or not
 
-    def __init__(self, parent=None):
+    def __init__(self, controller, parent=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
+        self._controller = controller
         self.parent = parent
 
         self.logger = Logger()
@@ -57,7 +58,7 @@ class XTFModelConverterDialog(QDialog, DIALOG_XTF_MODEL_CONVERTER_UI):
         self.tool_name = QCoreApplication.translate("XTFModelConverterDialog", "XTF Model Converter")
  
         # Initialize
-        self.initialize_feedback()
+        self.initialize_progress()
 
         # Set MessageBar for QDialog
         self.bar = QgsMessageBar()
@@ -91,31 +92,36 @@ class XTFModelConverterDialog(QDialog, DIALOG_XTF_MODEL_CONVERTER_UI):
 
         self.txt_source_xtf.textChanged.connect(self.validators.validate_line_edits)
         self.txt_target_xtf.textChanged.connect(self.validators.validate_line_edits)
-        
-        self.txt_source_xtf.textChanged.connect(self.input_data_changed)
-        self.txt_target_xtf.textChanged.connect(self.input_data_changed)
+
+        self.txt_source_xtf.textChanged.connect(self.update_model_converters)
+        self.txt_source_xtf.textChanged.connect(self.xtf_paths_changed)  # Enable/disable convert button
+        self.txt_target_xtf.textChanged.connect(self.xtf_paths_changed)  # Enable/disable convert button
+        self.cbo_model_converter.currentIndexChanged.connect(self.selected_converter_changed)  # Need new wizard pages?
 
         # Trigger validators now
         self.txt_source_xtf.textChanged.emit(self.txt_source_xtf.text())
         self.txt_target_xtf.textChanged.emit(self.txt_target_xtf.text())
 
-    def progress_configuration(self, base, num_process):
-        """
-        :param base: Where to start counting from
-        :param num_process: Number of steps
-        """
-        self.progress_base = base
-        self.progress_maximum = 100 * num_process
-        self.progress.setMaximum(self.progress_maximum)
-
-    def progress_changed(self):
+    def progress_changed(self, value):
         QCoreApplication.processEvents()  # Listen to cancel from the user
-        self.progress.setValue(self.progress_base + self.custom_feedback.progress())
+        self.progress.setValue(value)
 
     def accepted(self):
         self.save_settings()
-        # TODO: Call converter!
-        # self._controller.convert(self.txt_source_xtf.text(), self.txt_target_xtf.text(), converter, params)
+
+        self.set_gui_controls_enabled(False)
+
+        msg = QCoreApplication.translate("XTFModelConverterDialog", "Converting XTF data (this might take a while)...")
+        with ProcessWithStatus(msg):
+            params = {}
+            res, msg = self._controller.convert(self.cbo_model_converter.currentData(),
+                                                self.txt_source_xtf.text(),
+                                                self.txt_target_xtf.text(),
+                                                params)
+            self.show_message(msg, Qgis.Success if res else Qgis.Warning)
+            self.logger.success_warning(__name__, res, msg)
+
+        self.set_gui_controls_enabled(True)
 
     def reject(self):
         if self._running_tool:
@@ -126,7 +132,6 @@ class XTFModelConverterDialog(QDialog, DIALOG_XTF_MODEL_CONVERTER_UI):
                                          QMessageBox.Yes, QMessageBox.No)
 
             if reply == QMessageBox.Yes:
-                self.custom_feedback.cancel()
                 self._running_tool = False
                 msg = QCoreApplication.translate("XTFModelConverterDialog", "The '{}' tool was cancelled.").format(self.tool_name)
                 self.logger.info(__name__, msg)
@@ -138,29 +143,50 @@ class XTFModelConverterDialog(QDialog, DIALOG_XTF_MODEL_CONVERTER_UI):
     def finished_slot(self, result):
         self.bar.clearWidgets()
 
-    def input_data_changed(self):
-        state_source_xtf = self.txt_source_xtf.validator().validate(self.txt_source_xtf.text().strip(), 0)[0] == QValidator.Acceptable
+    def xtf_paths_changed(self):
+        # Enable/disable 'Convert' button
+        state_source_xtf = self.__source_xtf_is_valid()
         state_target_xtf = self.txt_target_xtf.validator().validate(self.txt_target_xtf.text().strip(), 0)[0] == QValidator.Acceptable
 
-        state_converter = True
+        state_converter = self.cbo_model_converter.count() and self.cbo_model_converter.currentData() != "invalid"
 
         self.set_convert_button_enabled(state_source_xtf and state_converter and state_target_xtf)
+
+    def __source_xtf_is_valid(self):
+        return self.txt_source_xtf.validator().validate(self.txt_source_xtf.text().strip(), 0)[0] == QValidator.Acceptable
+
+    def selected_converter_changed(self, index):
+        # Ideas for this:
+        #   Some converters might need new wizard pages. So this slot should get them from the controller
+        #   and pass them to a method that shows them, converting first the single-page wizard into multi-page.
+        pass
 
     def set_convert_button_enabled(self, enable):
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(enable)
 
-    def initialize_feedback(self):
+    def update_model_converters(self):
+        self.cbo_model_converter.clear()
+
+        if self.__source_xtf_is_valid():
+            self.cbo_model_converter.setEnabled(True)
+            source_xtf = self.txt_source_xtf.text().strip()
+            for key, text in self._controller.get_converters(source_xtf).items():
+                self.cbo_model_converter.addItem(text, key)
+
+            if not self.cbo_model_converter.count():
+                self.cbo_model_converter.addItem(QCoreApplication.translate("XTFModelConverterDialog",
+                                                                            "No converter found for the given source XTF"), "invalid")
+        else:
+            if not self.cbo_model_converter.count():
+                self.cbo_model_converter.setEnabled(False)
+
+    def initialize_progress(self):
         self.progress.setValue(0)
         self.progress.setVisible(False)
-        self.custom_feedback = CustomFeedback()
-        self.custom_feedback.progressChanged.connect(self.progress_changed)
-        self.set_gui_controls_enabled(False)
 
     def set_gui_controls_enabled(self, enable):
-        #if self.buttonBox.button(QDialogButtonBox.Ok) is not None:  # It's None if the tool finished successfully
-        #    self.set_convert_button_enabled(enable)
-        # self.gbx_parameters.setEnabled(enable)
-        pass
+        self.set_convert_button_enabled(enable)
+        self.gbx_parameters.setEnabled(enable)
 
     def save_settings(self):
         settings = QSettings()
@@ -180,6 +206,6 @@ class XTFModelConverterDialog(QDialog, DIALOG_XTF_MODEL_CONVERTER_UI):
             sizes = self.splitter.sizes()
             self.splitter.setSizes([sizes[0], 0])
         
-    def show_message(self, message, level):
+    def show_message(self, message, level, duration=0):
         self.bar.clearWidgets()  # Remove previous messages before showing a new one
-        self.bar.pushMessage(message, level, 15)
+        self.bar.pushMessage(message, level, duration)
