54da9d2972a9730c9f4d7ba5bc20d2bcce068c82
lacardonap <leo.cardona.p@gmail.com>
2022-06-07 13:50:02 -0500

    [qr] Migrate quality rule 'right with duplicate records' (IGAC-R4020)

--- asistente_ladm_col/core/quality_rules/quality_rule_registry.py
+++ asistente_ladm_col/core/quality_rules/quality_rule_registry.py
@@ -29,6 +29,7 @@ from asistente_ladm_col.logic.quality_rules.qr_parcel_right_relationship import 
 from asistente_ladm_col.logic.quality_rules.qr_parcel_with_invalid_parcel_number import QRParcelWithInvalidParcelNumber
 from asistente_ladm_col.logic.quality_rules.qr_parcel_department_field_valid_code import QRParcelWithInvalidDepartmentCode
 from asistente_ladm_col.logic.quality_rules.qr_administrative_source_with_duplicate_records import QRAdministrativeSourceWithDuplicateRecords
+from asistente_ladm_col.logic.quality_rules.qr_right_with_duplicate_records import QRRightWithDuplicateRecords
 from asistente_ladm_col.utils.singleton import Singleton
 
 
@@ -42,14 +43,15 @@ class QualityRuleRegistry(metaclass=Singleton):
         self.__quality_rules = dict()  # {quality_rule_key1: QualityRule1, ...}
 
         # Register default quality rules
-        self.register_quality_rule(QRValidateDataAgainstModel())
-        self.register_quality_rule(QROverlappingBoundaryPoints())
-        self.register_quality_rule(QROverlappingBoundaries())
-        self.register_quality_rule(QRGapsInPlots())
-        self.register_quality_rule(QRParcelRightRelationship())
-        self.register_quality_rule(QRParcelWithInvalidDepartmentCode())   # QR_IGACR4003
-        self.register_quality_rule(QRParcelWithInvalidParcelNumber())
-        self.register_quality_rule(QRAdministrativeSourceWithDuplicateRecords())
+        self.register_quality_rule(QRValidateDataAgainstModel())  # QR_ILIVALIDATORR0001
+        self.register_quality_rule(QROverlappingBoundaryPoints())  # QR_IGACR1001
+        self.register_quality_rule(QROverlappingBoundaries())  # QR_IGACR2001
+        self.register_quality_rule(QRGapsInPlots())  # QR_IGACR3006
+        self.register_quality_rule(QRParcelRightRelationship())  # QR_IGACR4001
+        self.register_quality_rule(QRParcelWithInvalidDepartmentCode())  # QR_IGACR4003
+        self.register_quality_rule(QRParcelWithInvalidParcelNumber())  # QR_IGACR4005
+        self.register_quality_rule(QRRightWithDuplicateRecords())  # QR_IGACR4020
+        self.register_quality_rule(QRAdministrativeSourceWithDuplicateRecords())  # QR_IGACR4022
 
     def register_quality_rule(self, quality_rule):
         """
---
+++ asistente_ladm_col/logic/quality_rules/qr_right_with_duplicate_records.py
@@ -0,0 +1,109 @@
+"""
+/***************************************************************************
+                              Asistente LADM_COL
+                             --------------------
+        begin                : 2022-06-07
+        git sha              : :%H$
+        copyright            : (C) 2022 by Leo Cardona (BSF Swissphoto)
+        email                : contacto@ceicol.com
+
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License v3.0 as          *
+ *   published by the Free Software Foundation.                            *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from qgis.PyQt.QtCore import (QCoreApplication,
+                              QSettings)
+
+from asistente_ladm_col.config.enums import (EnumQualityRuleType,
+                                             EnumQualityRuleResult)
+from asistente_ladm_col.config.keys.common import (QUALITY_RULE_LAYERS,
+                                                   QUALITY_RULE_LADM_COL_LAYERS,
+                                                   QUALITY_RULE_ADJUSTED_LAYERS,
+                                                   ADJUSTED_INPUT_LAYER,
+                                                   ADJUSTED_REFERENCE_LAYER,
+                                                   FIX_ADJUSTED_LAYER)
+from asistente_ladm_col.config.layer_config import LADMNames
+from asistente_ladm_col.config.quality_rule_config import (QR_IGACR4020,
+                                                           QRE_IGACR4020E01)
+from asistente_ladm_col.core.quality_rules.abstract_logic_quality_rule import AbstractLogicQualityRule
+from asistente_ladm_col.core.quality_rules.quality_rule_execution_result import QualityRuleExecutionResult
+from asistente_ladm_col.logic.ladm_col.ladm_data import LADMData
+
+
+class QRRightWithDuplicateRecords(AbstractLogicQualityRule):
+    """
+    Check that right don't have duplicate records
+    """
+    _ERROR_01 = QRE_IGACR4020E01  # Right with duplicate records
+
+    def __init__(self):
+        AbstractLogicQualityRule.__init__(self)
+
+        self._id = QR_IGACR4020
+        self._name = "Derecho no debe tener registros duplicados"
+        self._tags = ["igac", "instituto geográfico agustín codazzi", "lógica", "negocio", "derecho", "duplicado", "drr"]
+        self._models = [LADMNames.SURVEY_MODEL_KEY]
+
+        self._errors = {self._ERROR_01: "Derecho no debe tener registros repetidos"}
+
+        # Optional. Only useful for display purposes.
+        self._field_mapping = dict()  # E.g., {'id_objetos': 'ids_punto_lindero', 'valores': 'conteo'}
+
+    def layers_config(self, names):
+        return {QUALITY_RULE_LADM_COL_LAYERS: [names.LC_RIGHT_T]}
+
+    def _validate(self, db, db_qr, layer_dict, tolerance, **kwargs):
+        self.progress_changed.emit(5)
+        ladm_queries = self._get_ladm_queries(db.engine)
+
+        pre_res, pre_obj = self._check_prerrequisite_layers(layer_dict)
+        if not pre_res:
+            return pre_obj
+
+        error_state = None
+
+        table = db.names.LC_RIGHT_T
+        fields = [db.names.LC_RIGHT_T_TYPE_F,
+                  db.names.COL_RRR_T_DESCRIPTION_F,
+                  db.names.COL_RRR_PARTY_T_LC_GROUP_PARTY_F,
+                  db.names.COL_RRR_PARTY_T_LC_PARTY_F,
+                  db.names.COL_BAUNIT_RRR_T_UNIT_F]
+
+        res, records = ladm_queries.get_duplicate_records_in_table(db, table, fields)
+        count = len(records)
+
+        self.progress_changed.emit(50)
+
+        if res:
+            error_state = LADMData().get_domain_code_from_value(db_qr, db_qr.names.ERR_ERROR_STATE_D,
+                                                                LADMNames.ERR_ERROR_STATE_D_ERROR_V)
+            errors = {'geometries': list(), 'data': list()}
+            for record in records:
+                error_data = [  # [obj_uuids, rel_obj_uuids, values, details, state]
+                    record['duplicate_uuids'].split(','),
+                    None,
+                    None,
+                    'Derecho repetido {} veces'.format(record['duplicate_total']),
+                    error_state]
+                errors['data'].append(error_data)
+
+            self._save_errors(db_qr, self._ERROR_01, errors)
+
+        self.progress_changed.emit(85)
+
+        if count > 0:
+            res_type = EnumQualityRuleResult.ERRORS
+            msg = QCoreApplication.translate("QualityRules", "{} right "
+                                                             "with duplicate records were found.").format(count)
+        else:
+            res_type = EnumQualityRuleResult.SUCCESS
+            msg = QCoreApplication.translate("QualityRules", "No duplicate right were found.")
+
+        self.progress_changed.emit(100)
+
+        return QualityRuleExecutionResult(res_type, msg, count)
