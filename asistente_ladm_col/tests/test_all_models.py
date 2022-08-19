4a286e7e18549abbd5497be4a5790b80dff73b10
lacardonap <leo.cardona.p@gmail.com>
2022-08-19 12:37:07 -0500

    [tests] Use xtf file instead of database backup for test_all_models

--- asistente_ladm_col/tests/test_all_models.py
+++ asistente_ladm_col/tests/test_all_models.py
@@ -3,31 +3,34 @@ from abc import ABC
 
 import nose2
 
-from qgis.testing import start_app
-
-start_app() # need to start before asistente_ladm_col.tests.utils
-
 from asistente_ladm_col.lib.db.db_connector import DBConnector
 from asistente_ladm_col.tests.base_test_for_models import BaseTestForModels
 from asistente_ladm_col.tests.utils import (get_pg_conn,
-                                            get_gpkg_conn,
+                                            get_test_path,
                                             get_mssql_conn,
-                                            restore_schema_mssql,
-                                            reset_db_mssql,
-                                            restore_schema)
+                                            restore_pg_db,
+                                            restore_mssql_db,
+                                            restore_gpkg_db)
 
+from asistente_ladm_col.config.ladm_names import LADMNames
+from asistente_ladm_col.lib.model_registry import LADMColModelRegistry
 
 class BaseTestForAllModels(BaseTestForModels, ABC):
+    xtf_path = get_test_path("db/ladm/test_ladm_all_models_v1_2.xtf")
+    models = [LADMColModelRegistry().model(LADMNames.SURVEY_MODEL_KEY).full_name(),
+              LADMColModelRegistry().model(LADMNames.VALUATION_MODEL_KEY).full_name(),
+              LADMColModelRegistry().model(LADMNames.CADASTRAL_CARTOGRAPHY_MODEL_KEY).full_name()]
+
     def get_name_of_models(self):
         return 'All models'
 
     def check_required_models(self):
         self.assertTrue(self.db.supplies_model_exists())
-        # self.assertTrue(self.db.snr_data_model_exists())  # TODO: Migrate to Lev Cat 1.2
-        # self.assertTrue(self.db.supplies_integration_model_exists())  # TODO: Migrate to Lev Cat 1.2
-        # self.assertTrue(self.db.survey_model_exists())  # TODO: Migrate to Lev Cat 1.2
-        # self.assertTrue(self.db.valuation_model_exists())  # TODO: Migrate to Lev Cat 1.2
-        # self.assertTrue(self.db.cadastral_cartography_model_exists())  # TODO: Migrate to Lev Cat 1.2
+        self.assertTrue(self.db.snr_data_model_exists())
+        self.assertTrue(self.db.supplies_integration_model_exists())
+        self.assertTrue(self.db.survey_model_exists())
+        self.assertTrue(self.db.valuation_model_exists())
+        self.assertTrue(self.db.cadastral_cartography_model_exists())
 
 
 class TestAllModelsPG(BaseTestForAllModels, unittest.TestCase):
@@ -35,7 +38,7 @@ class TestAllModelsPG(BaseTestForAllModels, unittest.TestCase):
 
     @classmethod
     def restore_db(cls):
-        restore_schema(cls.schema)
+        restore_pg_db(cls.schema, cls.models, cls.xtf_path)
 
     @classmethod
     def get_connector(cls) -> DBConnector:
@@ -46,6 +49,7 @@ class TestAllModelsPG(BaseTestForAllModels, unittest.TestCase):
 
 
 class TestAllModelsGPKG(BaseTestForAllModels, unittest.TestCase):
+    file_name = 'test_ladm_all_models'
 
     def get_db_name(self):
         return 'GPKG'
@@ -56,7 +60,7 @@ class TestAllModelsGPKG(BaseTestForAllModels, unittest.TestCase):
 
     @classmethod
     def get_connector(cls) -> DBConnector:
-        return get_gpkg_conn('test_ladm_all_models_gpkg')
+        return restore_gpkg_db(cls.file_name, cls.models, cls.xtf_path)
 
 
 class TestAllModelsMSSQL(BaseTestForAllModels, unittest.TestCase):
@@ -67,8 +71,7 @@ class TestAllModelsMSSQL(BaseTestForAllModels, unittest.TestCase):
 
     @classmethod
     def restore_db(cls):
-        reset_db_mssql(cls.schema)
-        restore_schema_mssql(cls.schema)
+        restore_mssql_db(cls.schema, cls.models, cls.xtf_path)
 
     @classmethod
     def get_connector(cls) -> DBConnector:
